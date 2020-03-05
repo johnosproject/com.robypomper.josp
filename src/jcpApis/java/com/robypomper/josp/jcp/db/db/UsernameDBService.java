749c51f4ea463afbfb8b38b965c9ec1d9d541243
RobyPomper@John <robypomper@johnosproject.com>
2020-03-05 10:21:56 +0100

    feat(jcpAPIs): Added Spring support classes for Username Entity
    Spring use a Repository interface and a Service implementation as base connection classes to the database Entities.

---
+++ src/jcpApis/java/com/robypomper/josp/jcp/db/db/UsernameDBService.java
@@ -0,0 +1,33 @@
+package com.robypomper.josp.jcp.db.db;
+
+import com.robypomper.josp.jcp.db.entities.Username;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsernameDBService {
+
+    private final UsernameRepository usernames;
+
+    public UsernameDBService(UsernameRepository usernames) {
+        this.usernames = usernames;
+    }
+
+    public List<Username> findAll() {
+        return usernames.findAll();
+    }
+
+    public Optional<Username> findById(Long id) {
+        return usernames.findById(id);
+    }
+
+    public Username save(Username stock) {
+        return usernames.save(stock);
+    }
+
+    public void deleteById(Long id) {
+        usernames.deleteById(id);
+    }
+}
