dd7eb020e167e5aff6137163003d90cf9d4f610c
RobyPomper@John <robypomper@johnosproject.com>
2020-03-05 10:30:04 +0100

    feat(jcpAPIs): Moved the class @SpringBootApplication to com.robypomper.jcp package because Spring component scan

--- src/jcpApis/java/com/robypomper/josp/jcp/ServiceApplication.java
+++ src/jcpApis/java/com/robypomper/josp/jcp/ServiceApplication.java
@@ -1,7 +1,9 @@
-package com.robypomper.josp.jcp.apis;
+package com.robypomper.josp.jcp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * JCP API's service main class.
@@ -9,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * This class start a Spring Boot application that provide JCP API services.
  */
 @SpringBootApplication
+@Configuration
+@ComponentScan
 public class ServiceApplication {
 
     public static void main(String[] args) {
