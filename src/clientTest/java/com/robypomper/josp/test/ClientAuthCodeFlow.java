ffd1e20e70d5a0e8ec3a04decba0d6580c52ead1
RobyPomper@John <robypomper@johnosproject.com>
2020-03-09 15:18:08 +0100

    feat(clientTest): Added AbsClientFlow class and his implementations ClientAuthCodeFlow and ClientClientCredentialFlow
    The main AbsClientFlow contains the application runner (setup authentication and requests flow).
    The implementations contain the authentication details depending on represented flow.

---
+++ src/clientTest/java/com/robypomper/josp/test/ClientAuthCodeFlow.java
@@ -0,0 +1,42 @@
+package com.robypomper.josp.test;
+
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.github.scribejava.core.oauth.OAuth20Service;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
+
+public class ClientAuthCodeFlow extends AbsClientFlow {
+
+    public static final String AUTH_FLOW_NAME = "AuthCodeFlow";
+
+    public ClientAuthCodeFlow(ClientSettings clientSettings) {
+        super(clientSettings);
+    }
+
+    @Override
+    protected OAuth2AccessToken getAccessToken(OAuth20Service service)
+            throws InterruptedException, ExecutionException, IOException {
+        System.out.println("=== Auth Code Flow ===");
+
+        final Scanner in = new Scanner(System.in);
+
+        // Obtain the Authorization URL
+        //System.out.println("Fetching the Authorization URL...");
+        final String authorizationUrl = service.getAuthorizationUrl();
+        //System.out.println("Got the Authorization URL!");
+        System.out.println("Now go and authorize ScribeJava here:");
+        System.out.println(authorizationUrl);
+        System.out.println("And paste the authorization code here");
+        System.out.print(">>");
+        final String code = in.nextLine();
+
+        //System.out.println("Trading the Authorization Code for an Access Token...");
+        final OAuth2AccessToken accessToken = service.getAccessToken(code);
+        System.out.println(accessToken.getRawResponse());
+        return accessToken;
+    }
+
+}
