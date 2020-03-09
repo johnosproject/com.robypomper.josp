c921f464d8c1c9515d4f351559d86e32db67deef
RobyPomper@John <robypomper@johnosproject.com>
2020-03-09 23:39:13 +0100

    feat(jcpAPIs): Added JCPAPIsGroups dirs with placeholder controllers
    Added for each API group a dir and a placeholder java class.

---
+++ src/jcpAPIs_Service/java/com/robypomper/josp/jcp/apis/login/ServiceController.java
@@ -0,0 +1,65 @@
+package com.robypomper.josp.jcp.apis.login;
+
+import com.robypomper.josp.jcp.docs.SwaggerConfigurer;
+import com.robypomper.josp.jcp.info.JCPAPIsGroups;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Authorization;
+import io.swagger.annotations.AuthorizationScope;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
+
+@RestController
+@RequestMapping(JCPAPIsGroups.PATH_SRVS)
+@Api(tags = {JCPAPIsGroups.API_SRVS_SG_PLACEHOLDER_NAME})
+public class ServiceController {
+
+    @GetMapping("/public")
+    @ApiOperation("Public test method")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Method worked successfully", response = String.class)
+    })
+    public String getPublic() {
+        return "Service is up and running";
+    }
+
+    @GetMapping(path = "/private")
+    @ApiOperation(value = "Private test method",
+            authorizations = @Authorization(value=SwaggerConfigurer.OAUTH_FLOW_DEF_TEST)
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Method worked successfully", response = String.class),
+            @ApiResponse(code = 401, message = "User not authenticated")
+    })
+    @PreAuthorize("isAuthenticated()")
+    public String getPrivate() {
+        return "Service is up, running and private";
+    }
+
+    @GetMapping(path = "/role")
+    @ApiOperation(value = "Private and role protected test method",
+            authorizations = @Authorization(
+                    value=SwaggerConfigurer.OAUTH_FLOW_DEF_TEST,
+                    scopes = @AuthorizationScope(
+                            scope = SwaggerConfigurer.ROLE_OBJ_SWAGGER,
+                            description = SwaggerConfigurer.ROLE_OBJ_DESC
+                    )
+            )
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Method worked successfully", response = String.class),
+            @ApiResponse(code = 401, message = "User not authenticated"),
+            @ApiResponse(code = 403, message = "User not allowed to access")
+    })
+    @RolesAllowed(SwaggerConfigurer.ROLE_OBJ)
+    public String getRoleObj() {
+        return String.format("Service is up, running and protected (role: %s)", SwaggerConfigurer.ROLE_OBJ);
+    }
+
+}
