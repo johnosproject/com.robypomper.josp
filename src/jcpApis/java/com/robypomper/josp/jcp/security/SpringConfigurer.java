c8a953a232980b371940042b7fe7009c499a667f
RobyPomper@John <robypomper@johnosproject.com>
2020-03-09 16:06:21 +0100

    feat(jcpAPIs): Updated SpringConfigurer with OAuth2 authorization support
    OAuth is provided by SecurityConfigurerKeycloak and Keycloak adapter.

--- src/jcpApis/java/com/robypomper/josp/jcp/security/SpringConfigurer.java
+++ src/jcpApis/java/com/robypomper/josp/jcp/security/SpringConfigurer.java
@@ -1,12 +1,31 @@
 package com.robypomper.josp.jcp.security;
 
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @EnableWebSecurity
-public class SpringConfigurer extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
+@KeycloakConfiguration
+public class SpringConfigurer extends KeycloakWebSecurityConfigurerAdapter {
+
+    /** List of allowed public paths. */
+    @Value("${oauth2.resource.public-paths}")
+    private String[] mPublicPaths;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -17,7 +36,37 @@ public class SpringConfigurer extends WebSecurityConfigurerAdapter {
                 .requiresSecure()
 
                 // Enable CSRF token as cookie
+//                .and()
+//                .csrf()
+//                    .disable()
+//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+
+                // Enable Auth checks
                 .and()
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+                .authorizeRequests()
+                .antMatchers(mPublicPaths).permitAll()
+                .anyRequest().authenticated()
+
+                // Setup OAuth2 ResServer
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
     }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
+        SimpleAuthorityMapper grantedAuthorityMapper = new SimpleAuthorityMapper();
+        grantedAuthorityMapper.setPrefix("ROLE_");
+
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(grantedAuthorityMapper);
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
 }
