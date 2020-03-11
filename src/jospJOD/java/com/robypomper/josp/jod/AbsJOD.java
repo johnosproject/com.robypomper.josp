20e4a72dfe38978b93498f9f5ac19e7ccf6e84ce
RobyPomper@John <robypomper@johnosproject.com>
2020-03-11 14:40:56 +0100

    feat(jod): Added AbsJOD as default JOD implementation
    Added also the FactoryJOD support class.

---
+++ src/jospJOD/java/com/robypomper/josp/jod/AbsJOD.java
@@ -0,0 +1,202 @@
+package com.robypomper.josp.jod;
+
+import com.robypomper.josp.jod.systems.JODCommunication;
+import com.robypomper.josp.jod.systems.JODExecutorMngr;
+import com.robypomper.josp.jod.systems.JODObjectInfo;
+import com.robypomper.josp.jod.systems.JODPermissions;
+import com.robypomper.josp.jod.systems.JODStructure;
+
+/**
+ * Default JOD implementation, initialization excluded.
+ *
+ * This class fully manage a JOD object (start,stop,status...) for all JOD systems.
+ * But JOD systems initialization is delegate to his sub-classes. That allow to
+ * implement multiple JOD objects using different systems implementations. Helping
+ * develop new JOD versions and flavours.
+ *
+ * The JOD and {@link AbsJOD} hierarchy is design to allow sub-classes to initialize
+ * JOD systems (structure, comm, executor...) and delegate JOD systems orchestration
+ * to AbsJOD class. AbsJOD class manage JOD system using only their interfaces,
+ * that make system implementation completely interoperable (at JOD level). So
+ * AbsJOD sub-classes (like {@link JOD_002} can switch to different systems
+ * implementations/versions keeping full compatibility with all others of JOD
+ * systems.
+ *
+ * All AbsJOD sub-classes must implement the <code>instance(...)</code> method
+ * and return a self instance. <code>instance(...)</code> method can be
+ * implemented using {@link JOD.Settings} param or his sub-class.
+ * Returned class from {@link FactoryJOD#getJODClass(String)} must implement a
+ * <code>instance(...)</code> method with a param corresponding to class returned
+ * by {@link FactoryJOD#getJODSettingsClass(String)}. Both method are call using
+ * same String param corresponding to
+ */
+public abstract class AbsJOD implements JOD {
+
+    // Private systems references
+
+    private final JOD.Settings settings;
+    private final JODObjectInfo objInfo;
+    private final JODStructure structure;
+    private final JODCommunication comm;
+    private final JODExecutorMngr executor;
+    private final JODPermissions permissions;
+
+
+    // Internal vars
+
+    private Status status = Status.STOPPED;
+
+
+    // Constructor
+
+    /**
+     * Default constructor, set all private systems references.
+     *
+     * @param settings object containing current JOD configs.
+     * @param objInfo {@link JODObjectInfo} reference.
+     * @param structure {@link JODStructure} reference.
+     * @param comm {@link JODCommunication} reference.
+     * @param executor {@link JODExecutorMngr} reference.
+     * @param permissions {@link JODPermissions} reference.
+     */
+    protected AbsJOD(Settings settings, JODObjectInfo objInfo, JODStructure structure, JODCommunication comm, JODExecutorMngr executor, JODPermissions permissions) {
+        this.settings = settings;
+        this.objInfo = objInfo;
+        this.structure = structure;
+        this.comm = comm;
+        this.executor = executor;
+        this.permissions = permissions;
+    }
+
+
+    // JOD mngm
+
+    /**
+     * {@inheritDoc}
+     *
+     * Activate autoRefresh from {@link JODObjectInfo}, {@link JODPermissions}
+     * and {@link JODStructure} systems. Then activate the firmware's
+     * interfaces {@link com.robypomper.josp.jod.executor.JODPuller},
+     * {@link com.robypomper.josp.jod.executor.JODListener} and
+     * {@link com.robypomper.josp.jod.executor.JODExecutor}. Finally it start
+     * local and cloud communication.
+     *
+     * @throws RunException throw if current JOD object is already running.
+     */
+    @Override
+    public void start() throws RunException {
+        System.out.println("INF: JOD Obj startup...");
+
+        if (status()!=Status.STOPPED)
+            throw new RunException(String.format("Can't start JOD object because his state is '%s'.", status()));
+
+        if (status!=Status.REBOOTING) status = Status.STARTING;
+
+        // Info auto refresh
+        objInfo.startAutoRefresh();
+        permissions.startAutoRefresh();
+        structure.startAutoRefresh();
+
+        // HW interface
+        executor.activateAll();
+
+        // Service comm
+        comm.startCloudComm();
+        comm.startLocalComm();
+
+        if (status!=Status.REBOOTING) status = Status.RUNNING;
+        System.out.println("INF: JOD Obj is running.");
+        System.out.println(String.format("INF: JOD Obj version = %s", version()));
+        System.out.println(String.format("INF: JOD Obj Settings version = %s", settings.version()));
+        //System.out.println(String.format("INF: JOD Obj id = %s", objInfo.getObjId()));
+        //System.out.println(String.format("INF: JOD Obj name = %s", objInfo.getObjName()));
+        //System.out.println(String.format("INF: JOD Obj id hw = %s", objInfo.getObjIdHw()));
+        //System.out.println(String.format("INF: JOD Obj structure model = %s", structure.getRoot()...));
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * Stop local and cloud communication. Then deactivate the firmware's
+     * interfaces {@link com.robypomper.josp.jod.executor.JODPuller},
+     * {@link com.robypomper.josp.jod.executor.JODListener} and
+     * {@link com.robypomper.josp.jod.executor.JODExecutor}. Finally deactivate
+     * autoRefresh from {@link JODObjectInfo}, {@link JODPermissions}
+     * and {@link JODStructure} systems
+     *
+     * @throws RunException throw if current JOD object is already stopped.
+     */
+    @Override
+    public void stop() throws RunException {
+        System.out.println("INF: JOD Obj shutdown...");
+
+        if (status()!=Status.RUNNING)
+            throw new RunException(String.format("Can't stop JOD object because his state is '%s'.", status()));
+
+        if (status!=Status.REBOOTING) status = Status.SHUTDOWN;
+
+        // Service comm
+        comm.stopCloudComm();
+        comm.stopLocalComm();
+
+        // HW interface
+        executor.deactivateAll();
+
+        // Info auto refresh
+        objInfo.stopAutoRefresh();
+        permissions.stopAutoRefresh();
+        structure.stopAutoRefresh();
+
+        if (status!=Status.REBOOTING) status = Status.STOPPED;
+        System.out.println("INF: JOD Obj stopped.");
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws RunException thrown if errors occurs on JOD object stop and start.
+     */
+    @Override
+    public void restart() throws RunException {
+        status = Status.REBOOTING;
+        stop();
+        start();
+        status = Status.RUNNING;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Status status() {
+        return status;
+    }
+
+
+    // JOD Systems
+
+    @Override
+    public JODObjectInfo getObjectInfo() {
+        return objInfo;
+    }
+
+    @Override
+    public JODStructure getObjectStructure() {
+        return structure;
+    }
+
+    @Override
+    public JODCommunication getCommunication() {
+        return comm;
+    }
+
+    @Override
+    public JODExecutorMngr getExecutor() {
+        return executor;
+    }
+
+    @Override
+    public JODPermissions getPermission() {
+        return permissions;
+    }
+}
---
+++ src/jospJOD/java/com/robypomper/josp/jod/FactoryJOD.java
@@ -0,0 +1,160 @@
+package com.robypomper.josp.jod;
+
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.file.Paths;
+
+/**
+ * JOD Object factory.
+ *
+ * This class allow create JOD objects specifying their settings and JOD version.
+ *
+ * JOD and JOD.Settings versions must match.
+ *
+ * String versions are resolved with {@link JOD.Settings} implementation class
+ * or a {@link AbsJOD} sub-class by {@link #getJODSettingsClass(String)} and
+ * {@link #getJODSettingsClass(String)}.
+ */
+public class FactoryJOD {
+
+    // Version constants
+
+    public static final String JOD_VER_LATEST = "null";
+
+
+    // New instance method name
+
+    private static final String NEW_INSTANCE_METHOD = "instance";
+
+
+    // Factory methods
+
+    /**
+     * Create new JOD Settings object from given <code>fileName</code>.
+     *
+     * @param fileName file path of JOD config file.
+     * @return JOD Settings object.
+     */
+    public static JOD.Settings loadSettings(String fileName) throws JOD.FactoryException {
+        return loadSettings(fileName,"");
+    }
+
+    /**
+     * Create new JOD Settings object from given <code>fileName</code> and required
+     * <code>jodVer</code>.
+     *
+     * If <code>jodVer</code> is not empty, then his value will be updated on
+     * JOD.Settings loaded object. Otherwise {@link #JOD_VER_LATEST}
+     * ({@value #JOD_VER_LATEST}) version will be used.
+     *
+     * @param fileName file path of JOD config file.
+     * @param jodVer version corresponding to JOD.Settings implementation required.
+     * @return JOD Settings object.
+     */
+    public static JOD.Settings loadSettings(String fileName, String jodVer) throws JOD.FactoryException {
+        File file = Paths.get(fileName).toFile();
+        if (!file.exists()) throw new JOD.FactoryException(String.format("JOD config file '%s' not found.", file.getAbsolutePath()));
+        boolean updJODVerOnSettings = !jodVer.isEmpty();
+        if (jodVer.isEmpty()) jodVer = JOD_VER_LATEST;
+
+        Class<? extends JOD.Settings> jodSettingsClass = getJODSettingsClass(jodVer);
+
+        try {
+            Method method = jodSettingsClass.getMethod(NEW_INSTANCE_METHOD,file.getClass());
+            Object instance = method.invoke(null,file);
+            if (instance==null)
+                throw new JOD.FactoryException(String.format("JOD.Settings init method '%s::%s(%s)' return null object.", jodSettingsClass.getName(),NEW_INSTANCE_METHOD,file.getClass().getSimpleName()));
+            if (jodSettingsClass.isInstance(instance)) {
+                JOD.Settings settings = jodSettingsClass.cast(instance);
+                if (updJODVerOnSettings)
+                    settings.setJODVersion_Required(jodVer,false);
+                return settings;
+            }
+            if (instance instanceof JOD.Settings)
+                throw new JOD.FactoryException(String.format("JOD.Settings init method '%s::%s(%s)' return object of wrong sub-type '%s'.", jodSettingsClass.getName(),NEW_INSTANCE_METHOD,file.getClass().getSimpleName(),instance.getClass().getSimpleName()));
+            else
+                throw new JOD.FactoryException(String.format("JOD.Settings init method '%s::%s(%s)' return wrong object of type '%s'.", jodSettingsClass.getName(),NEW_INSTANCE_METHOD,file.getClass().getSimpleName(),instance.getClass().getSimpleName()));
+
+        } catch (NoSuchMethodException e) {
+            throw new JOD.FactoryException(String.format("JOD.Settings init method '%s::%s(%s)' not found.", jodSettingsClass.getName(),NEW_INSTANCE_METHOD,file.getClass().getSimpleName()),e);
+        } catch (IllegalAccessException e) {
+            throw new JOD.FactoryException(String.format("Can't access to JOD.Settings init method '%s::%s(%s)'.", jodSettingsClass.getName(),NEW_INSTANCE_METHOD,file.getClass().getSimpleName()),e);
+        } catch (InvocationTargetException e) {
+            throw new JOD.FactoryException(String.format("Error.Settings occurred during '%s::%s(%s)' JOD.Settings init method execution.", jodSettingsClass.getName(),NEW_INSTANCE_METHOD,file.getClass().getSimpleName()),e);
+        }
+    }
+
+    /**
+     * Create new JOD Object with given <code>settings</code>.
+     *
+     * @param settings JOD Settings object.
+     * @return JOD Object.
+     */
+    public static JOD createJOD(JOD.Settings settings) throws JOD.FactoryException {
+        return createJOD(settings,settings.getJODVersion_Required());
+    }
+
+    /**
+     * Create new JOD Object with given <code>settings</code> and required
+     * <code>jodVer</code>.
+     *
+     * @param settings JOD Settings object.
+     * @param jodVer version corresponding to JOD implementation required.
+     * @return JOD Object.
+     */
+    public static JOD createJOD(JOD.Settings settings, String jodVer) throws JOD.FactoryException {
+        if (settings==null) throw new JOD.FactoryException("JOD init method require Settings param");
+        if (jodVer.isEmpty()) jodVer = settings.getJODVersion_Required();
+
+        Class<? extends AbsJOD> jodClass = getJODClass(jodVer);
+
+        try {
+            Method method = jodClass.getMethod(NEW_INSTANCE_METHOD,settings.getClass());
+            Object instance = method.invoke(null,settings);
+            if (instance==null)
+                throw new JOD.FactoryException(String.format("JOD init method '%s::%s(%s)' return null object.", jodClass.getName(),NEW_INSTANCE_METHOD,settings.getClass().getSimpleName()));
+            if (instance instanceof JOD)
+                return (JOD)instance;
+            throw new JOD.FactoryException(String.format("JOD init method '%s::%s(%s)' return wrong object of type '%s'.", jodClass.getName(),NEW_INSTANCE_METHOD,settings.getClass().getSimpleName(),instance.getClass().getSimpleName()));
+
+        } catch (NoSuchMethodException e) {
+            throw new JOD.FactoryException(String.format("JOD init method '%s::%s(%s)' not found.", jodClass.getName(),NEW_INSTANCE_METHOD,settings.getClass().getSimpleName()),e);
+        } catch (IllegalAccessException e) {
+            throw new JOD.FactoryException(String.format("Can't access to JOD init method '%s::%s(%s)'.", jodClass.getName(),NEW_INSTANCE_METHOD,settings.getClass().getSimpleName()),e);
+        } catch (InvocationTargetException e) {
+            throw new JOD.FactoryException(String.format("Error occurred during '%s::%s(%s)' JOD init method execution.", jodClass.getName(),NEW_INSTANCE_METHOD,settings.getClass().getSimpleName()),e);
+        }
+    }
+
+
+    // Implementation class finders
+
+    /**
+     * <ul>
+     *     <li>
+     *         ...
+     *     </li>
+     * </ul>
+     *
+     * @param jodVer version corresponding to JOD.Settings implementation required.
+     * @return JOD.Settings class corresponding to given <code>jodVer</code> version.
+     */
+    private static Class<? extends JOD.Settings> getJODSettingsClass(String jodVer) throws JOD.FactoryException {
+        throw new JOD.FactoryException(String.format("JOD.Settings '%s' version not found.", jodVer));
+    }
+
+    /**
+     * <ul>
+     *     <li>
+     *         ...
+     *     </li>
+     * </ul>
+     *
+     * @param jodVer version corresponding to JOD implementation required.
+     * @return JOD class corresponding to given <code>jodVer</code> version.
+     */
+    private static Class<? extends AbsJOD> getJODClass(String jodVer) throws JOD.FactoryException {
+        throw new JOD.FactoryException(String.format("JOD.Settings '%s' version not found.", jodVer));
+    }
+}
