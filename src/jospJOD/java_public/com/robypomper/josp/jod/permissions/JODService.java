821405a22594457b07ef9a14cef68e63ff6ed3ec
RobyPomper@John <robypomper@johnosproject.com>
2020-03-11 14:08:48 +0100

    feat(jod): Added Permissions service interface
    Added also public JOD Permissions types (service and user) used to transmit entities from communication system.

---
+++ src/jospJOD/java_public/com/robypomper/josp/jod/permissions/JODService.java
@@ -0,0 +1,8 @@
+package com.robypomper.josp.jod.permissions;
+
+/**
+ * Service representation used by
+ * {@link com.robypomper.josp.jod.systems.JODPermissions} JOD system.
+ */
+public interface JODService {
+}
---
+++ src/jospJOD/java_public/com/robypomper/josp/jod/permissions/JODUser.java
@@ -0,0 +1,8 @@
+package com.robypomper.josp.jod.permissions;
+
+/**
+ * User representation used by
+ * {@link com.robypomper.josp.jod.systems.JODPermissions} JOD system.
+ */
+public interface JODUser {
+}
