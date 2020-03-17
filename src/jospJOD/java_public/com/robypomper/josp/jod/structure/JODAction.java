cdab391535404b13f773f69cae14dfc12f322f4e
RobyPomper@John <robypomper@johnosproject.com>
2020-03-17 19:02:53 +0100

    feat(jod): Implemented JOD structure components classes

--- src/jospJOD/java_public/com/robypomper/josp/jod/structure/JODAction.java
+++ src/jospJOD/java_public/com/robypomper/josp/jod/structure/JODAction.java
@@ -1,10 +1,34 @@
 package com.robypomper.josp.jod.structure;
 
+import com.robypomper.josp.jod.executor.JODExecutor;
+
+
 /**
  * Action component representation.
+ * <p>
+ * Action component receive commands from the JOD Communication system and elaborate
+ * them using associated {@link com.robypomper.josp.jod.executor.JODExecutor}.
  */
 public interface JODAction extends JODState {
 
-    void execAction(JODActionParams params);
+    // Status's properties
+
+    /**
+     * The string representing the executor that execute current component actions.
+     *
+     * @return the string representing current action's executor.
+     */
+    String getExecutor();
+
+
+    // Status upd flow (struct)
+
+    /**
+     * Called by JOD Communication system, this method execute the action via
+     * the {@link JODExecutor#exec()} method.
+     *
+     * @param params the action's to params.
+     */
+    boolean execAction(JODActionParams params);
 
 }
--- src/jospJOD/java_public/com/robypomper/josp/jod/structure/JODContainer.java
+++ src/jospJOD/java_public/com/robypomper/josp/jod/structure/JODContainer.java
@@ -2,6 +2,33 @@ package com.robypomper.josp.jod.structure;
 
 import java.util.Collection;
 
+
+/**
+ * Base container representation.
+ * <p>
+ * A container is a special component that can contain other components. This
+ * component is used to group and organize components across the object's structure.
+ * <p>
+ * This interface define methods to access to the sub components: getting the
+ * list of component's names or to get once by his name.
+ */
 public interface JODContainer extends JODComponent {
+
+    // Sub components
+
+    /**
+     * @return the list of all component's names contained in current container.
+     */
     Collection<JODComponent> getComponents();
+
+    /**
+     * Search the <code>name</code> component in the current container components
+     * list.
+     *
+     * @param name the name of searched component.
+     * @return the contained component or null if no component correspond to
+     * given <code>name</code>.
+     */
+    JODComponent getComponentByName(String name);
+
 }
