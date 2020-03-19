409ae70d82ad543c4ffe873012efa21c817e1c00
RobyPomper@John <robypomper@johnosproject.com>
2020-03-19 09:16:29 +0100

    feat(jospCommons): Create jospCommons and added ArgsGenerateObjId class

---
+++ src/jospCommons/java_public/com/robypomper/josp/jcp/apis/objs/data/ArgsGenerateObjId.java
@@ -0,0 +1,9 @@
+package com.robypomper.josp.jcp.apis.objs.data;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ArgsGenerateObjId {
+    public String objIdHw;
+    public String usrId;
+}
