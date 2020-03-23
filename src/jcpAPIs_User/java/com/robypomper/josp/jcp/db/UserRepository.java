c6cfcb7154bca1522585aa8ffcd1f26bce27d4fc
RobyPomper@John <robypomper@johnosproject.com>
2020-03-23 17:28:07 +0100

    refactor(jcp): Moved db classes from 'com.robypomper.josp.jcp.db.db' to 'com.robypomper.josp.jcp.db' package

--- src/jcpAPIs_User/java/com/robypomper/josp/jcp/db/UserRepository.java
+++ src/jcpAPIs_User/java/com/robypomper/josp/jcp/db/UserRepository.java
@@ -1,4 +1,4 @@
-package com.robypomper.josp.jcp.db.db;
+package com.robypomper.josp.jcp.db;
 
 import com.robypomper.josp.jcp.db.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
