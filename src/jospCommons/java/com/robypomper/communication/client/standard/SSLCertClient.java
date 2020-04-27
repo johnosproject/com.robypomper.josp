b2fe8a25b09a1c84f102566120b9d193c34ef130
RobyPomper@John <robypomper@johnosproject.com>
2020-04-27 14:35:28 +0200

    feat(jospCommons): Added SSLCertClient and his test
    Added also the COMM_SSL_CERTCLI log's marker.

---
+++ src/jospCommons/java/com/robypomper/communication/client/standard/SSLCertClient.java
@@ -0,0 +1,314 @@
+package com.robypomper.communication.client.standard;
+
+import com.robypomper.communication.UtilsJKS;
+import com.robypomper.communication.client.DefaultClient;
+import com.robypomper.communication.client.events.ClientMessagingEvents;
+import com.robypomper.communication.client.events.ClientServerEvents;
+import com.robypomper.communication.client.events.DefaultClientEvents;
+import com.robypomper.communication.client.events.LogClientLocalEventsListener;
+import com.robypomper.communication.server.ClientInfo;
+import com.robypomper.communication.server.standard.SSLCertServer;
+import com.robypomper.communication.trustmanagers.AbsCustomTrustManager;
+import com.robypomper.log.Markers;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+
+
+/**
+ * SSL sharing Certificate client.
+ * <p>
+ * This is a {@link DefaultClient} implementation that allow client to share
+ * SSL certificates with the server. When the client connects to the server, it
+ * send his public certificate (only if <code>certPubPath</code> was not null
+ * during SSLCertClient initialization. To the other side server send, always,
+ * his public certificate to the client when the connection is setup. Then the
+ * client store this certificate to the associated TrustManager.
+ * <p>
+ * The TrustManager managed by this class can be used to initialize a
+ * {@link javax.net.ssl.SSLContext} so a client can connect to a SSL server
+ * without previously key sharing.
+ */
+public class SSLCertClient extends DefaultClient {
+
+    // Internal vars
+
+    protected static final Logger log = LogManager.getLogger();
+    private final File certPubFile;
+    private final AbsCustomTrustManager certTrustManager;
+    private byte[] serverCertBuffer = new byte[0];
+
+    // Constructor
+
+    /**
+     * Default constructor for SSLCertClient instance that don't share his
+     * certificate (client side) with the server.
+     *
+     * @param clientId            the client id.
+     * @param serverAddr          the server's address.
+     * @param serverPort          the server's port.
+     * @param certTrustManager    instance of TrustManager where client's keys will
+     *                            be stored.
+     */
+    protected SSLCertClient(String clientId, InetAddress serverAddr, int serverPort, AbsCustomTrustManager certTrustManager) throws SSLCertClientException {
+        this(clientId,serverAddr,serverPort,null,certTrustManager);
+    }
+
+
+    /**
+     * Default constructor.
+     *
+     * @param clientId            the client id.
+     * @param serverAddr          the server's address.
+     * @param serverPort          the server's port.
+     * @param certPubPath         server's public certificate file's path, if
+     *                            <code>null</code> then the {@link SSLCertClient}
+     *                            don't send his certificate to the server.
+     * @param certTrustManager    instance of TrustManager where client's keys will
+     *                            be stored.
+     */
+    protected SSLCertClient(String clientId, InetAddress serverAddr, int serverPort, String certPubPath, AbsCustomTrustManager certTrustManager) throws SSLCertClientException {
+        super(clientId, serverAddr, serverPort,
+                new LogClientLocalEventsListener(),
+                new SSLCertClientServerEventsListener(),
+                new SSLCertClientMessagingEventsListener());
+
+        if (certPubPath!=null) {
+            this.certPubFile = new File(certPubPath);
+            if (!certPubFile.exists())
+                throw new SSLCertClientException(String.format("Public cert file '%s' not found.", certPubPath));
+        } else
+            this.certPubFile = null;
+        this.certTrustManager = certTrustManager;
+    }
+
+
+    // Clients's events override
+
+    /**
+     * This method is executed on event
+     * {@link SSLCertClientServerEventsListener#onServerConnection()}
+     * <p>
+     * Send client's public certificate to the connected server.
+     * <p>
+     * Read certificate file and tx to the server.
+     */
+    private void sendClientCertificate() {
+        if (certPubFile==null)
+            return;
+
+        try {
+            FileInputStream in = new FileInputStream(certPubFile);
+            byte[] dataRead = new byte[in.available()];
+
+            while (in.read(dataRead) != -1) {
+                //dataRead = trim(dataRead);
+                sendData(dataRead);
+            }
+            log.debug(Markers.COMM_SSL_CERTCLI, String.format("Client send local certificate to server '%s'", getServerInfo().getServerId()));
+            Thread.sleep(100);
+
+        } catch (ServerNotConnectedException e) {
+            log.warn(Markers.COMM_SSL_CERTCLI, String.format("Client disconnected, can't transmit local certificate to server '%s'", getServerInfo().getServerId()));
+
+        } catch (IOException e) {
+            log.warn(Markers.COMM_SSL_CERTCLI, String.format("Can't read public certificate '%s' because %s", certPubFile, e.getMessage()));
+
+        } catch (InterruptedException ignore) {
+        }
+    }
+
+    /**
+     * Data received from server are related to server's certificate.
+     * <p>
+     * Because it's possible that the server send his certificate in multiple
+     * chucks, this function use a buffer where store all sequential chucks
+     * received from the server. Then for each chuck received it try to load
+     * received bytes as certificate and then store it in the managed
+     * TrustManager. If it fails, then buffer received data and wait for next
+     * chuck.
+     *
+     * @param readData the data tx from the client containing the client's
+     *                 certificate.
+     * @return always <code>true</code> as the data were processed.
+     */
+    private boolean bufferServerCertificate(byte[] readData) {
+        serverCertBuffer = concatenate(serverCertBuffer, readData);
+        //System.out.println(String.format("Rx %d bytes", readData.length));
+        //System.out.println(String.format("%s -> %s", clientCertPath, new String(readData).substring(0, 10)));
+
+        if (tryStoreServerCertificate()) {
+            log.debug(Markers.COMM_SSL_CERTCLI, String.format("Client '%s' added certificate from server '%s' to trust store, disconnect", getClientId(), getServerInfo().getServerId()));
+            disconnect();
+        }
+
+        return true;
+    }
+
+    /**
+     * Support method for {@link #bufferServerCertificate(byte[])}
+     *
+     * Every time climent receive data from the server, this method is called
+     * and try to load certificate and store in the TrustManager.
+     */
+    private boolean tryStoreServerCertificate() {
+        if (serverCertBuffer.length == 0) {
+            log.debug(Markers.COMM_SSL_CERTCLI, String.format("Client '%s' don't received certificate from server '%s'", getClientId(), getServerInfo().getServerId()));
+            return false;
+        }
+
+        log.debug(Markers.COMM_SSL_CERTCLI, String.format("Client '%s' try add certificate from server '%s' to trust store", getClientId(), getServerInfo().getServerId()));
+
+        try {
+            certTrustManager.addCertificateByte(serverCertBuffer);
+            return true;
+
+        } catch (AbsCustomTrustManager.UpdateException | UtilsJKS.LoadingException e) {
+            log.debug(Markers.COMM_SSL_CERTCLI, String.format("Client '%s' can't add certificate from server '%s' because %s", getClientId(), getServerInfo().getServerId(), e.getMessage()));
+            return false;
+        }
+    }
+
+    /**
+     * Utils method to concatenate two byte arrays.
+     * <p>
+     * This method is a copy of {@link SSLCertServer#concatenate(byte[], byte[])}.
+     *
+     * @param firstArray  the first part of returned array.
+     * @param secondArray the second part of returned array.
+     * @return the byte array composed by firstArray and secondArray arrays.
+     */
+    @SuppressWarnings("JavadocReference")
+    private byte[] concatenate(byte[] firstArray, byte[] secondArray) {
+        byte[] concatenated = new byte[firstArray.length + secondArray.length];
+        System.arraycopy(firstArray, 0, concatenated, 0, firstArray.length);
+        System.arraycopy(secondArray, 0, concatenated, firstArray.length, secondArray.length);
+        return concatenated;
+    }
+
+
+    // Client event listeners
+
+    /**
+     * Link the {@link #onServerConnection()} event to
+     * {@link SSLCertClient#sendClientCertificate()} method.
+     */
+    private static class SSLCertClientServerEventsListener extends DefaultClientEvents implements ClientServerEvents {
+
+        // Server connection events
+
+        /**
+         * {@inheritDoc}
+         * <p>
+         * Link to the {@link SSLCertClient#sendClientCertificate()} method.
+         */
+        @Override
+        public void onServerConnection() {
+            ((SSLCertClient) getClient()).sendClientCertificate();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void onServerDisconnection() { }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void onServerClientDisconnected() {
+
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void onServerGoodbye() {
+
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void onServerTerminated() {
+
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void onServerError(Throwable e) {
+
+        }
+
+    }
+
+    /**
+     * Link the {@link #onDataReceived(byte[])} ()} event to
+     * {@link SSLCertClient#bufferServerCertificate(byte[])} method.
+     */
+    private static class SSLCertClientMessagingEventsListener extends DefaultClientEvents implements ClientMessagingEvents {
+
+        // Data send events
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void onDataSend(byte[] writtenData) {
+
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void onDataSend(String writtenData) {
+
+        }
+
+
+        // Data received events
+
+        /**
+         * {@inheritDoc}
+         * <p>
+         * Link to the {@link SSLCertClient#bufferServerCertificate(byte[])}
+         * method.
+         */
+        @Override
+        public boolean onDataReceived(byte[] readData) throws Throwable {
+            return ((SSLCertClient) getClient()).bufferServerCertificate(readData);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean onDataReceived(String readData) throws Throwable {
+            return false;
+        }
+
+    }
+
+
+    // Exception
+
+    /**
+     * Exception thrown on SSLCertServer errors.
+     */
+    public static class SSLCertClientException extends Throwable {
+        public SSLCertClientException(String msg) {
+            super(msg);
+        }
+    }
+
+}
---
+++ src/jospCommonsTest/java/com/robypomper/communication/client/standard/SSLCertClientTest.java
@@ -0,0 +1,161 @@
+package com.robypomper.communication.client.standard;
+
+import com.robypomper.communication.UtilsJKS;
+import com.robypomper.communication.client.Client;
+import com.robypomper.communication.server.Server;
+import com.robypomper.communication.server.standard.SSLCertServer;
+import com.robypomper.communication.trustmanagers.DynAddTrustManager;
+import com.robypomper.log.Markers;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.net.InetAddress;
+import java.security.KeyStore;
+
+
+public class SSLCertClientTest {
+
+    // Class constants
+
+    final static String ID_CLIENT = "TestCustomClient";
+    final static InetAddress LOCALHOST = InetAddress.getLoopbackAddress();
+    final static int PORT = 1234;
+    final static String ID_SERVER = "TestSSLCertServer";
+    final static String CLI_CERT_PUB_PATH = String.format("client-%s.crt", SSLCertClientTest.class.getSimpleName());
+    final static String SRV_CERT_PUB_PATH = String.format("server-%s.crt", SSLCertClientTest.class.getSimpleName());
+
+
+    // Internal vars
+
+    protected static Logger log = LogManager.getLogger();
+    protected Client clientSSLCert = null;
+    protected Client clientSSLCertSelfSend = null;
+    protected DynAddTrustManager clientCertTrustManager = null;
+    protected Server serverSSLCert = null;
+    protected DynAddTrustManager serverCertTrustManager = null;
+
+
+    // Test configurations
+
+    @BeforeAll
+    public static void setUpAll() throws UtilsJKS.GenerationException {
+        log.debug(Markers.TEST_SPACER, "########## ########## ########## ########## ##########");
+        log.debug(Markers.TEST_METHODS, "setUpAll");
+
+        log.debug(Markers.TEST, String.format("Generation of client keystore and export certificate to file '%s'", CLI_CERT_PUB_PATH));
+        KeyStore clientKs = UtilsJKS.generateKeyStore(ID_CLIENT, "ksPass", "clientCertificateAlias");
+        UtilsJKS.exportCertificate(clientKs, CLI_CERT_PUB_PATH, "clientCertificateAlias");
+
+        log.debug(Markers.TEST, String.format("Generation of server keystore and export certificate to file '%s'", SRV_CERT_PUB_PATH));
+        KeyStore serverKs = UtilsJKS.generateKeyStore(ID_SERVER, "ksPass", "serverCertificateAlias");
+        UtilsJKS.exportCertificate(serverKs, SRV_CERT_PUB_PATH, "serverCertificateAlias");
+    }
+
+    @AfterAll
+    public static void tearDownAll() {
+        log.debug(Markers.TEST_METHODS, "tearDownAll");
+
+        if (new File(CLI_CERT_PUB_PATH).delete())
+            log.debug(Markers.TEST, String.format("Public certificate file '%s' delete successfully", CLI_CERT_PUB_PATH));
+        else
+            log.debug(Markers.TEST, String.format("Error on deleting public certificate file '%s'", CLI_CERT_PUB_PATH));
+
+        if (new File(SRV_CERT_PUB_PATH).delete())
+            log.debug(Markers.TEST, String.format("Public certificate file '%s' delete successfully", SRV_CERT_PUB_PATH));
+        else
+            log.debug(Markers.TEST, String.format("Error on deleting public certificate file '%s'", SRV_CERT_PUB_PATH));
+    }
+
+    @BeforeEach
+    public void setUp() throws SSLCertServer.SSLCertServerException, SSLCertClient.SSLCertClientException {
+        log.debug(Markers.TEST_SPACER, "########## ########## ########## ########## ##########");
+        log.debug(Markers.TEST_METHODS, "setUp");
+
+        // Init test client
+        clientCertTrustManager = new DynAddTrustManager();
+        clientSSLCert = new SSLCertClient(ID_CLIENT, LOCALHOST, PORT, clientCertTrustManager);
+        clientSSLCertSelfSend = new SSLCertClient(ID_CLIENT, LOCALHOST, PORT, CLI_CERT_PUB_PATH, clientCertTrustManager);
+
+        // Init test server
+        serverCertTrustManager = new DynAddTrustManager();
+        serverSSLCert = new SSLCertServer(ID_SERVER, PORT, SRV_CERT_PUB_PATH, serverCertTrustManager);
+
+        log.debug(Markers.TEST_METHODS, "test");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        log.debug(Markers.TEST_METHODS, "tearDown");
+
+        // If still connected, disconnect test clients
+        if (clientSSLCert.isConnected())
+            clientSSLCert.disconnect();
+        if (clientSSLCertSelfSend.isConnected())
+            clientSSLCertSelfSend.disconnect();
+
+        // If still running, stop test server
+        if (serverSSLCert.isRunning())
+            serverSSLCert.stop();
+        serverCertTrustManager = null;
+    }
+
+
+    // isRunning, start and stop
+
+    @Test
+    public void testSSLCert() throws Server.ListeningException, InterruptedException, Client.ConnectionException {
+        // Start server and connect client
+        startServer(serverSSLCert);
+
+        clientSSLCert.connect();
+
+        Thread.sleep(100);
+
+        Assertions.assertEquals(1, clientCertTrustManager.getAcceptedIssuers().length);
+        Assertions.assertEquals(0, serverCertTrustManager.getAcceptedIssuers().length);
+
+        clientSSLCert.disconnect();
+
+        // Stop test server
+        stopServer(serverSSLCert);
+    }
+
+    @Test
+    public void testSSLCertClientSendSelf() throws Server.ListeningException, InterruptedException, Client.ConnectionException {
+        // Start server and connect client
+        startServer(serverSSLCert);
+
+        clientSSLCertSelfSend.connect();
+
+        Thread.sleep(100);
+
+        Assertions.assertEquals(1, clientCertTrustManager.getAcceptedIssuers().length);
+        Assertions.assertEquals(1, serverCertTrustManager.getAcceptedIssuers().length);
+
+        clientSSLCert.disconnect();
+
+        // Stop test server
+        stopServer(serverSSLCert);
+    }
+
+
+    // Utils methods
+
+    private void startServer(Server server) throws Server.ListeningException {
+        server.start();
+        assert server.isRunning();
+    }
+
+    private void stopServer(Server server) {
+        server.stop();
+        assert !server.isRunning();
+    }
+
+}
