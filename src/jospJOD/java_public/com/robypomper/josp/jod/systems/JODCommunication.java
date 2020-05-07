b9510ac8d7b3d39ad78c041c43807d5f683b1f2c
RobyPomper@John <robypomper@johnosproject.com>
2020-05-07 14:20:36 +0200

    feat(jod): Updated JODCommunication interface
    Added forwardAction(String) definition.
    Updated method definitions.
    Added jod comm's exceptions.
    Added connections getters definitions.

--- src/jospJOD/java_public/com/robypomper/josp/jod/systems/JODCommunication.java
+++ src/jospJOD/java_public/com/robypomper/josp/jod/systems/JODCommunication.java
@@ -1,16 +1,21 @@
 package com.robypomper.josp.jod.systems;
 
+import com.robypomper.josp.jod.comm.JODGwO2SClient;
+import com.robypomper.josp.jod.comm.JODLocalClientInfo;
 import com.robypomper.josp.jod.structure.JODState;
 import com.robypomper.josp.jod.structure.JODStateUpdate;
 
+import java.util.List;
+
+
 /**
  * Interface for Object's communication system.
- *
+ * <p>
  * The communication system provide channels for local and cloud communication.
- *
- * The local communication include a tcp server, his publication on the ZeroConf
- * protocol and local client management and security.
- *
+ * <p>
+ * The local communication include an SSL TCP server, his publication on the
+ * ZeroConf protocol and local client management.
+ * <p>
  * Cloud communication provide a channel to remote service via the JCP. When
  * activate, it start a tcp/ws socket to the JOSP GW on the JCP cloud. Then
  * the JOSP GW act as gateway from current object and remote services. Even
@@ -19,7 +24,7 @@ import com.robypomper.josp.jod.structure.JODStateUpdate;
  */
 public interface JODCommunication {
 
-    // Status upd flow (obj-comm)
+    // Status upd flow (objStruct - comm)
 
     /**
      * Dispatch <code>component</code> <code>update</code> to connected and allowed
@@ -34,36 +39,113 @@ public interface JODCommunication {
      */
     void dispatchUpdate(JODState component, JODStateUpdate update);
 
+    /**
+     * Parse and exec given action (in the <code>msg</code> string) to the right
+     * object's component.
+     */
+    void forwardAction(String msg);
+
+
+    // Connections access
+
+    /**
+     * @return the Gw O2S connection, null if not connected.
+     */
+    JODGwO2SClient getGwO2SClient();
+
+    /**
+     * @return an array containing all local connections.
+     */
+    List<JODLocalClientInfo> getAllLocalClientsInfo();
+
 
     // Mngm methods
 
     /**
+     * @return <code>true</code> if local communication server is running.
+     */
+    boolean isLocalRunning();
+
+    /**
      * Start local Object's server and publish it.
      */
-    void startLocalComm();
+    void startLocal() throws LocalCommunicationException;
 
     /**
      * Stop local Object's server and de-publish it, then close all opened connections.
      */
-    void stopLocalComm();
+    void stopLocal() throws LocalCommunicationException;
+
+    /**
+     * @return <code>true</code> if cloud communication server is connected.
+     */
+    boolean isCloudConnected();
 
     /**
      * Start JOSP Gw O2S Client.
      */
-    void startCloudComm();
+    void connectCloud() throws CloudCommunicationException;
 
     /**
      * Stop JOSP Gw O2S Client.
      */
-    void stopCloudComm();
+    void disconnectCloud();
 
+
+    // Cross component references
 
     /**
      * Set the {@link JODStructure} reference to the current object.
-     *
+     * <p>
      * This cross-system reference is required by the Action Execution Flow.
      *
      * @param structure the {@link JODStructure} reference.
      */
-    void setStructure(JODStructure structure);
+    void setStructure(JODStructure structure) throws StructureSetException;
+
+    /**
+     * @return the object's Structure sub system reference.
+     */
+    JODStructure getStructure() throws StructureSetException;
+
+
+    // Exceptions
+
+    /**
+     * Exceptions for {@link #setStructure(JODStructure)} called twice.
+     */
+    class StructureSetException extends Throwable {
+        private static final String MSG = "Structure already set for current Communication.";
+
+        public StructureSetException() {
+            super(MSG);
+        }
+    }
+
+    /**
+     * Exceptions for local communication errors.
+     */
+    class LocalCommunicationException extends Throwable {
+        public LocalCommunicationException(String msg) {
+            super(msg);
+        }
+
+        public LocalCommunicationException(String msg, Throwable e) {
+            super(msg, e);
+        }
+    }
+
+    /**
+     * Exceptions for cloud communication errors.
+     */
+    class CloudCommunicationException extends Throwable {
+        public CloudCommunicationException(String msg) {
+            super(msg);
+        }
+
+        public CloudCommunicationException(String msg, Throwable e) {
+            super(msg, e);
+        }
+    }
+
 }
