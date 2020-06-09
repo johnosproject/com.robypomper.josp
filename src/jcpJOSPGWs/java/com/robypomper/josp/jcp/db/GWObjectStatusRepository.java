1568e2ba2d223391fc494e14c1da062493535561
RobyPomper@John <robypomper@johnosproject.com>
2020-06-09 17:48:44 +0200

    feat(jcpAPIGWs): Added GWObjectStatus entity to APIs GWs
    Added GWObjectStatus's repository and service.

---
+++ src/jcpJOSPGWs/java/com/robypomper/josp/jcp/db/GWObjectStatusRepository.java
@@ -0,0 +1,6 @@
+package com.robypomper.josp.jcp.db;
+
+import com.robypomper.josp.jcp.db.entities.GWObjectStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GWObjectStatusRepository extends JpaRepository<GWObjectStatus, String> {}
