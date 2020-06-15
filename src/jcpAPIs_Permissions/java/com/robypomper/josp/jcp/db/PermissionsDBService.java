9083587ec68324bb180bbc699d975ef69364a7a6
RobyPomper@John <robypomper@johnosproject.com>
2020-06-15 16:22:21 +0200

    refactor(jcp): Moved getObjectPresentationAllowed() method from ServiceDBService to PermissionDBService
    Removed getObjectPresentationAllowed() method from ServiceDBService class
    Added getObjectPresentationAllowed() and isObjectActionAllowed() methods to PermissionDBService

--- src/jcpAPIs_Permissions/java/com/robypomper/josp/jcp/db/PermissionsDBService.java
+++ src/jcpAPIs_Permissions/java/com/robypomper/josp/jcp/db/PermissionsDBService.java
@@ -1,11 +1,15 @@
 package com.robypomper.josp.jcp.db;
 
+import com.robypomper.josp.jcp.apis.params.permissions.PermissionsTypes;
+import com.robypomper.josp.jcp.db.entities.Object;
 import com.robypomper.josp.jcp.db.entities.Permission;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.SessionScope;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -15,12 +19,14 @@ public class PermissionsDBService {
     // Internal vars
 
     private final PermissionRepository permissions;
+    private final ObjectRepository objects;
 
 
     // Constructor
 
-    public PermissionsDBService(PermissionRepository permissions) {
+    public PermissionsDBService(PermissionRepository permissions, ObjectRepository objects) {
         this.permissions = permissions;
+        this.objects = objects;
     }
 
 
@@ -38,12 +44,45 @@ public class PermissionsDBService {
         return permissions.findByUsrId(usrId);
     }
 
-    public List<Permission> addAll(List<Permission> objPerms) {
-        return permissions.saveAll(objPerms);
+    public void addAll(List<Permission> objPerms) {
+        objPerms = permissions.saveAll(objPerms);
     }
 
     public void removeAll(List<Permission> objPerms) {
         permissions.deleteAll(objPerms);
+    }
+
+
+    // Permission queries
+
+    public List<Object> getObjectStatusAllowed(String srvId, String usrId) {
+        String[] types = new String[]{
+                PermissionsTypes.Type.Status.name(),
+                PermissionsTypes.Type.Actions.name(),
+                PermissionsTypes.Type.CoOwner.name()
+        };
+        List<Permission> ps = permissions.findForSrv(PermissionsTypes.Connection.LocalAndCloud.name(), types, srvId, usrId);
+
+        List<Object> objs = new ArrayList<>();
+        for (Permission p : ps) {
+            Optional<Object> optObj = objects.findById(p.getObjId());
+            optObj.ifPresent(objs::add);
+        }
+        return objs;
+    }
+
+    public boolean isObjectActionAllowed(String objId, String srvId, String usrId) {
+        String[] types = new String[]{
+                PermissionsTypes.Type.Actions.name(),
+                PermissionsTypes.Type.CoOwner.name()
+        };
+        List<Permission> ps = permissions.findForSrv(PermissionsTypes.Connection.LocalAndCloud.name(), types, srvId, usrId);
+
+        for (Permission p : ps)
+            if (p.getObjId().equals(objId))
+                return true;
+
+        return false;
     }
 
 }
--- src/jcpAPIs_Service/java/com/robypomper/josp/jcp/db/ServiceDBService.java
+++ src/jcpAPIs_Service/java/com/robypomper/josp/jcp/db/ServiceDBService.java
@@ -1,14 +1,9 @@
 package com.robypomper.josp.jcp.db;
 
-import com.robypomper.josp.jcp.apis.params.permissions.PermissionsTypes;
-import com.robypomper.josp.jcp.db.entities.Object;
-import com.robypomper.josp.jcp.db.entities.Permission;
 import com.robypomper.josp.jcp.db.entities.Service;
 import com.robypomper.josp.jcp.db.entities.ServiceStatus;
 import org.springframework.dao.DataIntegrityViolationException;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 
@@ -19,16 +14,12 @@ public class ServiceDBService {
 
     private final ServiceRepository services;
     private final ServiceStatusRepository servicesStatus;
-    private final ObjectRepository objects;
-    private final PermissionRepository permissions;
 
     // Constructor
 
-    public ServiceDBService(ServiceRepository services, ServiceStatusRepository servicesStatus, ObjectRepository objects, PermissionRepository permissions) {
+    public ServiceDBService(ServiceRepository services, ServiceStatusRepository servicesStatus) {
         this.services = services;
         this.servicesStatus = servicesStatus;
-        this.objects = objects;
-        this.permissions = permissions;
     }
 
 
@@ -50,23 +41,6 @@ public class ServiceDBService {
     public void save(ServiceStatus srvStatus) throws DataIntegrityViolationException {
         srvStatus = servicesStatus.save(srvStatus);
         // ToDo check references returned
-    }
-
-
-    public List<Object> getObjectPresentationAllowed(String srvId, String usrId) {
-        String[] types = new String[]{
-                PermissionsTypes.Type.Status.name(),
-                PermissionsTypes.Type.Actions.name(),
-                PermissionsTypes.Type.CoOwner.name()
-        };
-        List<Permission> ps = permissions.findForSrv(PermissionsTypes.Connection.LocalAndCloud.name(), types, srvId, usrId);
-
-        List<Object> objs = new ArrayList<>();
-        for (Permission p : ps) {
-            Optional<Object> optObj = objects.findById(p.getObjId());
-            optObj.ifPresent(objs::add);
-        }
-        return objs;
     }
 
 }
