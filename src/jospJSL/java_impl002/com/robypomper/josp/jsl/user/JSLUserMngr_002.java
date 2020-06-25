7dda953dca7925682121102baef5b24a7bbe9172
RobyPomper@John <robypomper@johnosproject.com>
2020-06-25 16:52:07 +0200

    fix(jsl): Updated reference from PermissionsTypes to JOSPPermissions

--- src/jospJSL/java_impl002/com/robypomper/josp/jsl/user/JSLUserMngr_002.java
+++ src/jospJSL/java_impl002/com/robypomper/josp/jsl/user/JSLUserMngr_002.java
@@ -1,10 +1,10 @@
 package com.robypomper.josp.jsl.user;
 
 import com.robypomper.josp.core.jcpclient.JCPClient;
-import com.robypomper.josp.jcp.apis.params.permissions.PermissionsTypes;
 import com.robypomper.josp.jsl.JSLSettings_002;
 import com.robypomper.josp.jsl.comm.JSLCommunication;
 import com.robypomper.josp.jsl.jcpclient.JCPClient_Service;
+import com.robypomper.josp.protocol.JOSPPermissions;
 import com.robypomper.log.Mrk_JSL;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -17,8 +17,8 @@ public class JSLUserMngr_002 implements JSLUserMngr, JCPClient_Service.LoginMana
 
     // Class constants
 
-    public static final String ANONYMOUS_ID = PermissionsTypes.WildCards.USR_ANONYMOUS_ID.toString();
-    public static final String ANONYMOUS_USERNAME = PermissionsTypes.WildCards.USR_ANONYMOUS_NAME.toString();
+    public static final String ANONYMOUS_ID = JOSPPermissions.WildCards.USR_ANONYMOUS_ID.toString();
+    public static final String ANONYMOUS_USERNAME = JOSPPermissions.WildCards.USR_ANONYMOUS_NAME.toString();
 
 
     // Internal vars
