0f78fe878d6bd7a3e0fb2dac8ef85c86bcb1f7f0
RobyPomper@John <robypomper@johnosproject.com>
2020-06-26 14:00:36 +0200

    refactor(josp): Merged classes JOSPPermission and ObjPermission in JOSPPerm
    Removed ObjPermission and JOSPPermissions classes
    Moved JOSPPerm class from JOSPProtocol_ObjectToService class to his file JOSPPerm.java

--- src/jospCommons/java_public/com/robypomper/josp/protocol/JOSPProtocol_ObjectToService.java
+++ src/jospCommons/java_public/com/robypomper/josp/protocol/JOSPProtocol_ObjectToService.java
@@ -1,7 +1,6 @@
 package com.robypomper.josp.protocol;
 
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class JOSPProtocol_ObjectToService {
@@ -93,7 +92,6 @@ public class JOSPProtocol_ObjectToService {
     public static final String OBJ_PERMS_REQ_NAME = "ObjectPerms";
     private static final String OBJ_PERMS_REQ_BASE = JOSPProtocol.JOSP_PROTO + " OBJ_PERMS_MSG";
     private static final String OBJ_PERMS_REQ = OBJ_PERMS_REQ_BASE + " %s\nobjId:%s\n%s";
-    private static final String OBJ_PERMS_REQ_FORMAT = "objId:%s;srvId:%s;usrId%s;type:%s;conn:%s";
 
     public static String createObjectPermsMsg(String objId, String perms) {
         return String.format(OBJ_PERMS_REQ, JOSPProtocol.getNow(), objId, perms);
@@ -104,79 +102,8 @@ public class JOSPProtocol_ObjectToService {
     }
 
     public static List<JOSPPerm> getObjectPermsMsg_Perms(String msg) throws JOSPProtocol.ParsingException {
-        List<JOSPPerm> perms = new ArrayList<>();
-
         String permsStr = JOSPProtocol.extractPayloadFromResponse(msg, 3, 2, OBJ_PERMS_REQ_NAME);
-        for (String permStr : permsStr.split("\n"))
-            perms.add(JOSPPerm.fromString(permStr));
-
-        return perms;
-    }
-
-    public static class JOSPPerm {
-
-        private final String objId;
-        private final String srvId;
-        private final String usrId;
-        private final String permType;
-        private final String permConn;
-
-        public JOSPPerm(String objId, String srvId, String usrId, String permType, String permConn) {
-            this.objId = objId;
-            this.srvId = srvId;
-            this.usrId = usrId;
-            this.permType = permType;
-            this.permConn = permConn;
-        }
-
-        public String getObjId() {
-            return objId;
-        }
-
-        public String getSrvId() {
-            return srvId;
-        }
-
-        public String getUsrId() {
-            return usrId;
-        }
-
-        public String getPermType() {
-            return permType;
-        }
-
-        public String getPermConn() {
-            return permConn;
-        }
-
-        public static JOSPPerm fromString(String permStr) throws JOSPProtocol.ParsingException {
-            String[] permStrs = permStr.split(";");
-            if (permStrs.length != 5)
-                throw new JOSPProtocol.ParsingException("Few fields in JOSPPerm string");
-
-            String objId = permStrs[0].substring(0, permStrs[0].indexOf(":"));
-            String srvId = permStrs[1].substring(0, permStrs[1].indexOf(":"));
-            String usrId = permStrs[2].substring(0, permStrs[2].indexOf(":"));
-            String permType = permStrs[3].substring(0, permStrs[3].indexOf(":"));
-            String permConn = permStrs[4].substring(0, permStrs[4].indexOf(":"));
-
-            return new JOSPPerm(objId, srvId, usrId, permType, permConn);
-        }
-
-        public static String toString(JOSPPerm perm) {
-            return String.format(OBJ_PERMS_REQ_FORMAT, perm.getObjId(), perm.getSrvId(), perm.getUsrId(), perm.getPermType(), perm.getPermConn());
-        }
-
-        public static String toString(List<JOSPPerm> perms) {
-            StringBuilder str = new StringBuilder();
-            for (JOSPPerm perm : perms) {
-                str.append(toString(perm));
-                str.append("\n");
-            }
-
-            return str.toString();
-        }
-
+        return JOSPPerm.listFromString(permsStr);
     }
 
 
@@ -186,7 +113,7 @@ public class JOSPProtocol_ObjectToService {
     private static final String SRV_PERMS_REQ_BASE = JOSPProtocol.JOSP_PROTO + " SRV_PERMS_MSG";
     private static final String SRV_PERMS_REQ = SRV_PERMS_REQ_BASE + " %s\nobjId:%s\npermType:%s\npermConn:%s";
 
-    public static String createServicePermMsg(String objId, JOSPPermissions.Type permType, JOSPPermissions.Connection permConn) {
+    public static String createServicePermMsg(String objId, JOSPPerm.Type permType, JOSPPerm.Connection permConn) {
         return String.format(SRV_PERMS_REQ, JOSPProtocol.getNow(), objId, permType, permConn);
     }
 
