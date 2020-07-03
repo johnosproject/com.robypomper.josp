8a780253e7bf904b6b5a32670c60cd5454a8e6b1
RobyPomper@John <robypomper@johnosproject.com>
2020-07-03 14:20:26 +0200

    refactor(jcp): Remove unused api constants
    Removed domains, FULL_PATH_XY_BASE and URL_PATH_XY_BASE constants from JcpAPI class.
    Removed URL_PATH_BASE and URL_PATH_XY constants rom APIAuth, APIJOSPGWs, APIObjs, APIPermissions, APISrvs and APIUsrs classes.

--- src/jcpApisPublic/java_public/com/robypomper/josp/jcp/apis/paths/APIAuth.java
+++ src/jcpApisPublic/java_public/com/robypomper/josp/jcp/apis/paths/APIAuth.java
@@ -23,8 +23,6 @@ public class APIAuth {
 
     public static final String FULL_PATH_BASE   = JcpAPI.PATH_AUTH_BASE + PATH_BASE;
 
-    public static final String URL_PATH_BASE    = JcpAPI.URL_PATH_AUTH_BASE + PATH_BASE;
-
 
     // Keycloak APIs
 
@@ -32,22 +30,14 @@ public class APIAuth {
 
     public static final String FULL_PATH_USER   = FULL_PATH_BASE + PATH_USER;
 
-    public static final String URL_PATH_USER    = URL_PATH_BASE + PATH_USER;
-
 
     // Auth service
 
     public static final String PATH_AUTH        = "/realms/" + REALM + "/protocol/openid-connect/auth";
     public static final String PATH_TOKEN       = "/realms/" + REALM + "/protocol/openid-connect/token";
-    public static final String PATH_CALLBACK    = "/callback";
 
     public static final String FULL_PATH_AUTH       = FULL_PATH_BASE + PATH_AUTH;
     public static final String FULL_PATH_TOKEN      = FULL_PATH_BASE + PATH_TOKEN;
-    public static final String FULL_PATH_CALLBACK   = "/callback";
-
-    public static final String URL_PATH_AUTH        = URL_PATH_BASE + PATH_AUTH;
-    public static final String URL_PATH_TOKEN       = URL_PATH_BASE + PATH_TOKEN;
-    public static final String URL_PATH_CALLBACK    = JcpAPI.URL_DOM_API + PATH_CALLBACK;
 
 //@formatter:on
 }
--- src/jcpApisPublic/java_public/com/robypomper/josp/jcp/apis/paths/APISrvs.java
+++ src/jcpApisPublic/java_public/com/robypomper/josp/jcp/apis/paths/APISrvs.java
@@ -10,14 +10,10 @@ public class APISrvs {
 
     public static final String FULL_PATH_BASE   = JcpAPI.PATH_API_BASE + PATH_BASE;
 
-    public static final String URL_PATH_BASE    = JcpAPI.URL_PATH_API_BASE + PATH_BASE;
-
 
     public static final String PATH_REGISTER        = "/" + version + "/service";
 
     public static final String FULL_PATH_REGISTER   = FULL_PATH_BASE + PATH_REGISTER;
-
-   public static final String URL_PATH_REGISTER     = URL_PATH_BASE + PATH_REGISTER;
 
 
     public static final String HEADER_SRVID = "JOSP-Srv-ID";
