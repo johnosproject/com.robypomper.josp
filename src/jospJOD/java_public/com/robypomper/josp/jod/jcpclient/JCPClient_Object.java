0129b56f197e4b811a86f8de2671381c48a1ff24
RobyPomper@John <robypomper@johnosproject.com>
2020-07-03 11:35:06 +0200

    refactor(jod): Updated reference from JCPClient to JCPClient2 on JOD
    Updated JCPClient_Object interface and his DefaultJCPClient_Object implementation to inherit from JCPClient2.
    Removed connection and connectionTimer management from DefaultJCPClient_Object.
    Updated AbsJCPAPIs class's references from JCPClient to JCPClient2.
    Updated instance() method of JOD_002 class to initialize and connect the JCPClient2 object.
    Added JCP_URL_APIS and JCP_URL_AUTH properties to JODSettings_002.
    Updated JOD configs files with new properties JCP_URL_APIS and JCP_URL_AUTH.

--- src/jospJOD/java_public/com/robypomper/josp/jod/jcpclient/JCPClient_Object.java
+++ src/jospJOD/java_public/com/robypomper/josp/jod/jcpclient/JCPClient_Object.java
@@ -1,13 +1,13 @@
 package com.robypomper.josp.jod.jcpclient;
 
-import com.robypomper.josp.core.jcpclient.JCPClient;
+import com.robypomper.josp.core.jcpclient.JCPClient2;
 import com.robypomper.josp.jcp.apis.paths.APIObjs;
 
 
 /**
  * Main JCP client interface for Objects.
  */
-public interface JCPClient_Object extends JCPClient {
+public interface JCPClient_Object extends JCPClient2 {
 
     // Headers default values setters
 
