9e7b20022ea1d3eac0b08fbface94406a0b8c286
Roberto Pompermaier <robypomper@johnosproject.com>
2020-07-20 11:53:51 +0200

    feat(jospCommons): Added JavaVersionUtils class with buildJavaVersionStr() static method
    It will used to print JOSP Component and Java execution versions and other environment vars.

---
+++ src/jospCommons/java/com/robypomper/java/JavaVersionUtils.java
@@ -0,0 +1,32 @@
+package com.robypomper.java;
+
+import java.io.File;
+
+public class JavaVersionUtils {
+
+    public static String buildJavaVersionStr(String mainAppName, String mainAppVersion) {
+        return mainAppName + " versions:   " + mainAppVersion + "\n" +
+                "+-----------------\n" +
+                "| Current dir:     " + new File("").getAbsolutePath() + "\n" +
+                "+-----------------\n" +
+                "| Java Version:    " + System.getProperty("java.specification.version") + "(" + System.getProperty("java.version") + ")" + "\n" +
+                "| Java VM Version: " + System.getProperty("java.vm.specification.version") + "\n" +
+                "| Java Runtime:    " + System.getProperty("java.runtime.name") + "\n" +
+                "| Java Home:       " + System.getProperty("java.home") + "\n" +
+                "| Java Home (ENV): " + System.getenv("JAVA_HOME") + "\n" +
+                "| Java Vendor:     " + System.getProperty("java.vm.vendor") + "\n" +
+                "| Java Libs Path:  " + System.getProperty("java.library.path") + "\n" +
+                "| Java ClassPath:  " + System.getProperty("java.class.path") + "\n" +
+                "+-----------------\n" +
+                "| OS Name:         " + System.getProperty("os.name") + "\n" +
+                "| OS Version:      " + System.getProperty("os.version") + "\n" +
+                "| Current user:    " + System.getProperty("user.name") + "\n" +
+                "| Dflt language:   " + System.getProperty("user.language") + "\n" +
+                "| Dflt encoding:   " + System.getProperty("sun.jnu.encoding") + "\n" +
+                "| File encoding:   " + System.getProperty("file.encoding") + "\n" +
+                "+-----------------\n" +
+                "| Working dir:     " + System.getProperty("user.dir") + "\n" +
+                "+-----------------\n";
+    }
+
+}
