94fdca948768137abeb85d175b7db5d2a2db2bcf
Roberto Pompermaier <robypomper@johnosproject.com>
2020-07-30 16:27:09 +0200

    feat(josp): Added specific method to covert Boolean and Double to String

--- src/jospCommons/java/com/robypomper/java/JavaFormatter.java
+++ src/jospCommons/java/com/robypomper/java/JavaFormatter.java
@@ -2,19 +2,44 @@ package com.robypomper.java;
 
 import java.text.NumberFormat;
 import java.text.ParseException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+
 
 public class JavaFormatter {
 
+    // Independent comparison for Boolean
 
-    // Independent locale formatter
+    public static final List<String> TRUE_ALIASES = Arrays.asList("TRUE", "1", "ON", "HIGH", "OPEN", "FULL");
 
-    public static final Locale LOCALE_STANDARD = Locale.US;
+
+    // Independent locale formatter for Double
+
     public static final Locale LOCALE_POINT_DEC = Locale.US;
     public static final Locale LOCALE_COMMA_DEC = Locale.ITALY;
+    public static final Locale LOCALE_STANDARD = LOCALE_POINT_DEC;
+
+
+    // Boolean convert methods
+
+    public static boolean strToBoolean(String s) {
+        return TRUE_ALIASES.contains(s);
+    }
+
+
+    // Double convert methods
 
     public static String doubleToStr(double d) {
         return String.format(LOCALE_STANDARD, "%f", d);
+    }
+
+    public static String doubleToStr_Point(double d) {
+        return String.format(LOCALE_POINT_DEC, "%f", d);
+    }
+
+    public static String doubleToStr_Comma(double d) {
+        return String.format(LOCALE_COMMA_DEC, "%f", d);
     }
 
     public static Double strToDouble(String s) {
