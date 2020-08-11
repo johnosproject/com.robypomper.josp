fafcea184bd303dff5c8441d0d5ef2f33a9cb631
Roberto Pompermaier <robypomper@johnosproject.com>
2020-08-11 12:28:36 +0200

    feat(jcpFE): Added JCP FE's REST API for ObjsMngr
    Added ObjsMngrController as Spring's controller class.
    Added APIJCPFEObjs as API's path classes.
    Added JOSPObjHtml as API's param classes.

---
+++ src/jcpFE/java/com/robypomper/josp/jcpfe/apis/ObjsMngrController.java
@@ -0,0 +1,173 @@
+package com.robypomper.josp.jcpfe.apis;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.robypomper.josp.jcpfe.HTMLUtils;
+import com.robypomper.josp.jcpfe.apis.params.JOSPObjHtml;
+import com.robypomper.josp.jcpfe.apis.paths.APIJCPFEObjs;
+import com.robypomper.josp.jcpfe.jsl.JSLSpringService;
+import com.robypomper.josp.jsl.objs.JSLRemoteObject;
+import com.robypomper.josp.protocol.JOSPPerm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+//@Api(tags = {APIJCPFEObjs.SubGroupObjs.NAME})
+public class ObjsMngrController {
+
+    // Internal var
+
+    @Autowired
+    private JSLSpringService jslService;
+
+
+    // List and details
+
+    public static List<JOSPObjHtml> objectsList(JSLSpringService jslStaticService) {
+        // Convert object list
+        List<JOSPObjHtml> objHtml = new ArrayList<>();
+        for (JSLRemoteObject o : jslStaticService.listObjects())
+            objHtml.add(new JOSPObjHtml(o));
+
+        return objHtml;
+    }
+
+    @GetMapping(path = APIJCPFEObjs.FULL_PATH_LIST)
+    public ResponseEntity<List<JOSPObjHtml>> jsonObjectsList() {
+        return ResponseEntity.ok(objectsList(jslService));
+    }
+
+    @GetMapping(path = APIJCPFEObjs.FULL_PATH_LIST, produces = MediaType.TEXT_HTML_VALUE)
+    public String htmlObjectsList() {
+        List<JOSPObjHtml> objHtml = jsonObjectsList().getBody();
+        if (objHtml == null)
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error on get '%s' objects list.");
+
+        try {
+            return HTMLUtils.toHTMLFormattedJSON(objHtml, "Object's List");
+
+        } catch (JsonProcessingException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, String.format("Error get objects list on formatting response (%s).", e.getMessage()), e);
+        }
+    }
+
+    @GetMapping(path = APIJCPFEObjs.FULL_PATH_DETAILS)
+    public ResponseEntity<JOSPObjHtml> jsonObjectDetails(@PathVariable("obj_id") String objId) {
+        JSLRemoteObject obj = jslService.getObj(objId);
+        return ResponseEntity.ok(new JOSPObjHtml(obj));
+    }
+
+    @GetMapping(path = APIJCPFEObjs.FULL_PATH_DETAILS, produces = MediaType.TEXT_HTML_VALUE)
+    public String htmlObjectDetails(@PathVariable("obj_id") String objId) {
+        JOSPObjHtml objHtml = jsonObjectDetails(objId).getBody();
+        if (objHtml == null)
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, String.format("Error on get '%s' object.", objId));
+
+        try {
+            return HTMLUtils.toHTMLFormattedJSON(objHtml,
+                    String.format("%s Object", jslService.getObj(objId).getName()),
+                    String.format("<a href=\"%s\">Object</a>", APIJCPFEObjs.FULL_PATH_DETAILS(objId)));
+
+        } catch (JsonProcessingException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, String.format("Error get '%s' object's permissions on formatting response (%s).", objId, e.getMessage()), e);
+        }
+    }
+
+
+    // Set owner and name
+
+    @GetMapping(path = APIJCPFEObjs.FULL_PATH_OWNER, produces = MediaType.TEXT_HTML_VALUE)
+    public String formObjectOwner(@PathVariable("obj_id") String objId) {
+        // ONLY HTML
+
+        return "<form id = \"form_id\" method=\"post\">\n" +
+                "    <input type=\"text\" id=\"new_owner\" name=\"new_owner\" value=\"" + jslService.getObj(objId).getOwnerId() + "\">\n" +
+                "    <input type=\"submit\" value=\"Set\">\n" +
+                "</form>\n" +
+                "<form id = \"form_id_anonymous\" method=\"post\">\n" +
+                "    <input type=\"hidden\" id=\"new_owner\" name=\"new_owner\" value=\"" + JOSPPerm.WildCards.USR_ANONYMOUS_ID + "\">\n" +
+                "    <input type=\"submit\" value=\"Set Anonymous Owner\">\n" +
+                "</form>\n" +
+                "<form id = \"form_id_self\" method=\"post\">\n" +
+                "    <input type=\"hidden\" id=\"new_owner\" name=\"new_owner\" value=\"" + jslService.getUserMngr().getUserId() + "\">\n" +
+                "    <input type=\"submit\" value=\"Set current user\">\n" +
+                "</form>\n";
+    }
+
+    @PostMapping(path = APIJCPFEObjs.FULL_PATH_OWNER)
+    public ResponseEntity<Boolean> jsonObjectOwner(@PathVariable("obj_id") String objId,
+                                                   @RequestParam("new_owner") String newOwner) {
+        JSLRemoteObject obj = jslService.getObj(objId);
+
+        // Check permission (Preventive)
+        if (jslService.getObjPerm(obj) != JOSPPerm.Type.CoOwner)
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, String.format("Permission denied to current user/service on update permission to '%s' object.", objId));
+
+        try {
+            obj.setOwnerId(newOwner);
+
+        } catch (JSLRemoteObject.MissingPermission e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, String.format("Permission denied to current user/service on set owner to '%s' object.", objId), e);
+
+        } catch (JSLRemoteObject.ObjectNotConnected e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, String.format("Can't send 'set owner' message because '%s' object is not connected.", objId), e);
+        }
+
+        return ResponseEntity.ok(true);
+    }
+
+    @PostMapping(path = APIJCPFEObjs.FULL_PATH_OWNER, produces = MediaType.TEXT_HTML_VALUE)
+    public String htmlObjectOwner(@PathVariable("obj_id") String objId,
+                                  @RequestParam("new_owner") String newOwner) {
+        Boolean success = jsonObjectOwner(objId, newOwner).getBody();
+        success = success != null && success;
+        return HTMLUtils.redirectAndReturn(APIJCPFEObjs.FULL_PATH_DETAILS(objId), success);
+    }
+
+    @GetMapping(path = APIJCPFEObjs.FULL_PATH_NAME, produces = MediaType.TEXT_HTML_VALUE)
+    public String formObjectRename(@PathVariable("obj_id") String objId) {
+        // ONLY HTML
+
+        return "<form id = \"form_id\" method=\"post\">\n" +
+                "    <input type=\"text\" id=\"new_name\" name=\"new_name\" value=\"\n" + jslService.getObj(objId).getName() + "\">\n" +
+                "    <input type=\"submit\" value=\"Set\">\n" +
+                "</form>\n" +
+                "</script>";
+    }
+
+    @PostMapping(path = APIJCPFEObjs.FULL_PATH_NAME)
+    public ResponseEntity<Boolean> jsonObjectName(@PathVariable("obj_id") String objId,
+                                                  @RequestParam("new_name") String newName) {
+
+        JSLRemoteObject obj = jslService.getObj(objId);
+        try {
+            obj.setName(newName);
+
+        } catch (JSLRemoteObject.MissingPermission e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, String.format("Permission denied to current user/service on rename '%s' object.", objId), e);
+
+        } catch (JSLRemoteObject.ObjectNotConnected e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, String.format("Can't send rename message because '%s' object is not connected.", objId), e);
+        }
+
+        return ResponseEntity.ok(true);
+    }
+
+    @PostMapping(path = APIJCPFEObjs.FULL_PATH_NAME, produces = MediaType.TEXT_HTML_VALUE)
+    public String htmlObjectName(@PathVariable("obj_id") String objId,
+                                 @RequestParam("new_name") String newName) {
+        Boolean success = jsonObjectName(objId, newName).getBody();
+        success = success != null && success;
+        return HTMLUtils.redirectAndReturn(APIJCPFEObjs.FULL_PATH_DETAILS(objId), success);
+    }
+
+
+    // Shared structures
+
+}
---
+++ src/jcpFE/java/com/robypomper/josp/jcpfe/apis/params/JOSPObjHtml.java
@@ -0,0 +1,42 @@
+package com.robypomper.josp.jcpfe.apis.params;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.robypomper.josp.jcpfe.apis.paths.APIJCPFEObjs;
+import com.robypomper.josp.jcpfe.apis.paths.APIJCPFEPermissions;
+import com.robypomper.josp.jcpfe.apis.paths.APIJCPFEStructure;
+import com.robypomper.josp.jsl.objs.JSLRemoteObject;
+
+@JsonAutoDetect
+public class JOSPObjHtml {
+
+    public final String id;
+    public final String name;
+    public final String owner;
+    public final boolean isConnected;
+    public final boolean isCloudConnected;
+    public final boolean isLocalConnected;
+    public final String jodVersion;
+    public final String pathSingle;
+    public final String pathStruct;
+    public final String pathPerms;
+    public final String pathPermsAdd;
+    public final String pathSetOwner;
+    public final String pathSetName;
+
+    public JOSPObjHtml(JSLRemoteObject obj) {
+        this.id = obj.getId();
+        this.name = obj.getName();
+        this.owner = obj.getOwnerId();
+        this.isConnected = obj.isConnected();
+        this.isCloudConnected = obj.isCloudConnected();
+        this.isLocalConnected = obj.isLocalConnected();
+        this.jodVersion = obj.getJODVersion();
+        this.pathSingle = APIJCPFEObjs.FULL_PATH_DETAILS.replace("{obj_id}", id);
+        this.pathStruct = APIJCPFEStructure.FULL_PATH_STRUCT.replace("{obj_id}", id);
+        this.pathPerms = APIJCPFEPermissions.FULL_PATH_LIST.replace("{obj_id}", id);
+        this.pathPermsAdd = APIJCPFEPermissions.FULL_PATH_ADD.replace("{obj_id}", id);
+        this.pathSetOwner = APIJCPFEObjs.FULL_PATH_OWNER.replace("{obj_id}", id);
+        this.pathSetName = APIJCPFEObjs.FULL_PATH_NAME.replace("{obj_id}", id);
+    }
+
+}
