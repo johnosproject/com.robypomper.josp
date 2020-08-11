7020d3f036c00bac89b0b41bab4cb330588f4ac1
Roberto Pompermaier <robypomper@johnosproject.com>
2020-08-11 13:12:56 +0200

    feat(jcpFE): Added SingleJSLInstanceTest as temporary home page for JCP FE REST API's

---
+++ src/jcpFE/java/com/robypomper/josp/jcpfe/apis/SingleJSLInstanceTest.java
@@ -0,0 +1,85 @@
+package com.robypomper.josp.jcpfe.apis;
+
+import com.robypomper.josp.jcpfe.apis.params.JOSPObjHtml;
+import com.robypomper.josp.jcpfe.apis.params.JOSPSrvHtml;
+import com.robypomper.josp.jcpfe.apis.params.JOSPUserHtml;
+import com.robypomper.josp.jcpfe.apis.paths.APIJCPFEObjs;
+import com.robypomper.josp.jcpfe.apis.paths.APIJCPFEService;
+import com.robypomper.josp.jcpfe.apis.paths.APIJCPFEUser;
+import com.robypomper.josp.jcpfe.jsl.JSLSpringService;
+import com.robypomper.josp.protocol.JOSPProtocol_Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test/")
+public class SingleJSLInstanceTest {
+
+    // Internal vars
+
+    @Autowired
+    private JSLSpringService jslService;
+
+
+    // List
+
+    @GetMapping(path = "/", produces = MediaType.TEXT_HTML_VALUE)
+    public String htmlHome() {
+        StringBuilder responseBuilder = new StringBuilder();
+        responseBuilder.append("<h1>JCP Front-End</h1>\n");
+        responseBuilder.append("<div style=\"float: left;\">\n");
+
+        // Objects
+        responseBuilder.append("<p><b>Objects:</b></p>\n");
+        responseBuilder.append("<ul>\n");
+        for (JOSPObjHtml o : ObjsMngrController.objectsList(jslService))
+            responseBuilder.append("<li>").append("<a href=\"").append(o.pathSingle).append("\">").append(o.name).append("</a>").append("</li>\n");
+        responseBuilder.append("</ul>\n");
+        responseBuilder.append(String.format("Go to the <a href=\"%s\">full Objects list</a> (%d objects)\n", APIJCPFEObjs.FULL_PATH_LIST, ObjsMngrController.objectsList(jslService).size()));
+        responseBuilder.append("<hr>\n");
+
+        // User
+        responseBuilder.append("<p><b>Current user:</b></p>\n");
+        JOSPUserHtml user = UserController.userDetails(jslService);
+        responseBuilder.append("<p>");
+        responseBuilder.append("User: ").append(user.name).append("(");
+        responseBuilder.append(String.format("<a href=\"%s\">details</a>)<br>\n", APIJCPFEUser.FULL_PATH_DETAILS));
+        if (!user.isAuthenticated)
+            responseBuilder.append(String.format("<a href=\"%s\">Login</a><br>\n", APIJCPFEUser.FULL_PATH_LOGIN));
+        else
+            responseBuilder.append(String.format("<a href=\"%s\">Logout</a><br>\n", APIJCPFEUser.FULL_PATH_LOGOUT));
+        responseBuilder.append("</p>\n");
+        responseBuilder.append("<hr>\n");
+
+        // Service
+        responseBuilder.append("<p><b>Service:</b></p>\n");
+        JOSPSrvHtml srv = ServiceController.serviceDetails(jslService);
+        responseBuilder.append("<p>");
+        responseBuilder.append("Srv: ").append(srv.name).append("(");
+        responseBuilder.append(String.format("<a href=\"%s\">details</a>)<br>\n", APIJCPFEService.FULL_PATH_DETAILS));
+        responseBuilder.append("ID: ").append(String.format(JOSPProtocol_Service.FULL_SRV_ID_FORMAT, srv.srvId, srv.usrId, srv.instId)).append("<br>\n");
+        responseBuilder.append("</p>\n");
+
+
+        responseBuilder.append("</div>");
+
+        // Login box
+        responseBuilder.append("<div style=\"float: right;\">\n");
+        if (!user.isAuthenticated) {
+            responseBuilder.append("User <b>Anonymous</b><br>");
+            responseBuilder.append(String.format("<a href=\"%s\">Login</a><br>\n", APIJCPFEUser.FULL_PATH_LOGIN));
+        } else {
+            responseBuilder.append(String.format("User <b>%s</b><br>", user.name));
+            responseBuilder.append(String.format("<a href=\"%s\">Logout</a><br>\n", APIJCPFEUser.FULL_PATH_LOGOUT));
+        }
+
+        responseBuilder.append("</div>\n");
+
+
+        return responseBuilder.toString();
+    }
+
+}
