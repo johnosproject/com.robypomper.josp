a1190c2442b7504a71c21e0882cc02915b43e1b3
Roberto Pompermaier <robypomper@johnosproject.com>
2020-10-07 21:49:06 +0200

    build(Gradle/buildSrc): Added makeSpringBootFromSourceSet with logConfigs param to SpringBuildUtils class

--- buildSrc/src/main/java/com/robypomper/build/spring/SpringBuildUtils.java
+++ buildSrc/src/main/java/com/robypomper/build/spring/SpringBuildUtils.java
@@ -28,12 +28,13 @@ import org.gradle.api.tasks.SourceSet;
 import org.springframework.boot.gradle.tasks.bundling.BootJar;
 import org.springframework.boot.gradle.tasks.run.BootRun;
 
+import java.io.File;
 import java.util.Collections;
 
 
 /**
  * Add support to Spring Boot applications.
- *
+ * <p>
  * This class register 2 tasks to build and run Spring Boot applications.
  */
 public class SpringBuildUtils {
@@ -45,14 +46,20 @@ public class SpringBuildUtils {
      * @param ss
      */
     static public void makeSpringBootFromSourceSet(Project project, SourceSet ss) {
-        SpringBuildUtils.configureBootJarTask(project,ss);
-        SpringBuildUtils.configureBootRunTask(project,ss);
+        SpringBuildUtils.configureBootJarTask(project, ss);
+        SpringBuildUtils.configureBootRunTask(project, ss);
+    }
+
+    static public void makeSpringBootFromSourceSet(Project project, SourceSet ss, File logConfigs) {
+        SpringBuildUtils.configureBootJarTask(project, ss);
+        BootRun br = SpringBuildUtils.configureBootRunTask(project, ss);
+        br.setJvmArgs(Collections.singletonList("-Dlogging.config=" + logConfigs.getAbsolutePath()));
     }
 
     static private BootJar configureBootJarTask(Project project, SourceSet ss) {
         String taskName = String.format("boot%sJar", Naming.capitalize(ss.getName()));
         BootJar bootJar = project.getTasks().create(taskName, BootJar.class);
-        bootJar.setDescription(String.format("Assembles an executable jar archive containing the %s classes and their dependencies.",ss.getName()));
+        bootJar.setDescription(String.format("Assembles an executable jar archive containing the %s classes and their dependencies.", ss.getName()));
         bootJar.setGroup(BasePlugin.BUILD_GROUP);
         bootJar.classpath(ss.getRuntimeClasspath());
         //bootJar.conventionMapping(String.format("%sClassName", ss.getName()), new MainClassConvention2(project, bootJar::getClasspath));
@@ -65,7 +72,7 @@ public class SpringBuildUtils {
         String buildTaskName = String.format("boot%sJar", Naming.capitalize(ss.getName()));
         JavaPluginConvention javaConvention = project.getConvention().getPlugin(JavaPluginConvention.class);
         BootRun run = project.getTasks().create(taskName, BootRun.class);
-        run.setDescription(String.format("Runs this project %s sources as a Spring Boot application.",ss.getName()));
+        run.setDescription(String.format("Runs this project %s sources as a Spring Boot application.", ss.getName()));
         run.setGroup(ApplicationPlugin.APPLICATION_GROUP);
         run.classpath(ss.getRuntimeClasspath());
         run.getConventionMapping().map("jvmArgs", () -> {
