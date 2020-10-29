dc7adacabef9603e72c913b731b958c6a5d9f058
Roberto Pompermaier <robypomper@johnosproject.com>
2020-10-29 18:19:52 +0100

    feat(tests): Updated JOSPStructIntegration test with injected JOD's History in JOD's structure
    (cherry picked from commit c5c29b17c1f546ce1d41f8b20eed8e4eb269eaf2)

---
+++ tests/test_Commons/java/com/robypomper/josp/test/mocks/jod/MockJODHistory.java
@@ -0,0 +1,42 @@
+/* *****************************************************************************
+ * The John Object Daemon is the agent software to connect "objects"
+ * to an IoT EcoSystem, like the John Operating System Platform one.
+ * Copyright (C) 2020 Roberto Pompermaier
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **************************************************************************** */
+
+package com.robypomper.josp.test.mocks.jod;
+
+import com.robypomper.josp.jod.history.JODHistory;
+import com.robypomper.josp.jod.structure.JODComponent;
+import com.robypomper.josp.jod.structure.JODStateUpdate;
+
+import java.io.IOException;
+
+public class MockJODHistory implements JODHistory {
+
+    @Override
+    public void register(JODComponent comp, JODStateUpdate update) {}
+
+    @Override
+    public void startCloudSync() {}
+
+    @Override
+    public void stopCloudSync() {}
+
+    @Override
+    public void storeCache() throws IOException {}
+
+}
