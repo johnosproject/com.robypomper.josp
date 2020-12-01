1db903d2c7e8c5abf75ad7bccc4b8a9c8e88687c
Roberto Pompermaier <robypomper@johnosproject.com>
2020-12-01 09:59:50 +0100

    feat(jospCommons): Added JavaThread utils class with softSleep() method

---
+++ src/jospCommons/java/com/robypomper/java/JavaThreads.java
@@ -0,0 +1,14 @@
+package com.robypomper.java;
+
+public class JavaThreads {
+
+    public static boolean softSleep(long millis) {
+        try {
+            Thread.sleep(millis);
+            return true;
+        } catch (InterruptedException e) {
+            return false;
+        }
+    }
+
+}
