3838c0c93c5737a02c81f33bc6f1112f6346db8d
Roberto Pompermaier <robypomper@johnosproject.com>
2020-12-16 12:42:38 +0100

    feat(jospCommons): Added JavaEnum utils class with Synchronizable and SynchronizableStatus subclasses
    Added also Mrk_Commons::STATE because SynchronizableState prints state's updates with that marker.

---
+++ src/jospCommons/java/com/robypomper/java/JavaEnum.java
@@ -0,0 +1,95 @@
+package com.robypomper.java;
+
+import com.robypomper.log.Mrk_Commons;
+import org.apache.logging.log4j.Logger;
+
+public class JavaEnum {
+
+    public static class Synchronizable<E> {
+
+        // Internal vars
+
+        private E val;
+
+
+        // Constructor
+
+        public Synchronizable(E val) {
+            this.val = val;
+        }
+
+
+        // Value access
+
+        public void set(E val) {
+            this.val = val;
+        }
+
+        public E get() {
+            return val;
+        }
+
+
+        // Value comparison
+
+        public boolean enumEquals(E obj) {
+            return this.val == obj;
+        }
+
+        public boolean enumNotEquals(E obj) {
+            return !enumEquals(obj);
+        }
+
+    }
+
+    public static class SynchronizableState<E> extends Synchronizable<E> {
+
+        // Internal vars
+
+        private Logger log;
+        private String logName;
+
+
+        // Constructor
+
+        public SynchronizableState(E val) {
+            this(val, Thread.currentThread().getStackTrace()[2].getClassName().substring(Thread.currentThread().getStackTrace()[2].getClassName().lastIndexOf('.') + 1), null);
+        }
+
+        public SynchronizableState(E val, String logName) {
+            this(val, logName, null);
+        }
+
+        public SynchronizableState(E val, Logger log) {
+            this(val, Thread.currentThread().getStackTrace()[2].getClassName().substring(Thread.currentThread().getStackTrace()[2].getClassName().lastIndexOf('.') + 1), log);
+        }
+
+        public SynchronizableState(E val, String logName, Logger log) {
+            super(val);
+            this.log = log;
+            this.logName = logName;
+        }
+
+
+        // Value access
+
+        public void set(E val) {
+            set(log, val);
+        }
+
+        public void set(Logger log, E val) {
+            if (log != null)
+                log.debug(Mrk_Commons.STATE, String.format("%s state = %s", logName, val));
+            super.set(val);
+        }
+
+
+        // Setters
+
+        public void setLogName(String logName) {
+            this.logName = logName;
+        }
+
+    }
+
+}
