45142631d4c4db7ecce4f7fbe4e52c1a6068e2f7
Roberto Pompermaier <robypomper@johnosproject.com>
2021-02-26 15:00:58 +0100

    fix(jcpServices): Updated CORS configs
    Added POST" and "OPTIONS" to allowed methods.

--- src/jcpService/java/com/robypomper/josp/jcp/service/spring/SpringConfigurerAuth.java
+++ src/jcpService/java/com/robypomper/josp/jcp/service/spring/SpringConfigurerAuth.java
@@ -41,6 +41,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @EnableWebSecurity
@@ -124,6 +125,7 @@ public class SpringConfigurerAuth extends KeycloakWebSecurityConfigurerAdapter {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
