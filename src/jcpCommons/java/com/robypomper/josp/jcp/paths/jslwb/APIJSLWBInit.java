c43625d24fc095636dba24ee932842499ecb74ec
Roberto Pompermaier <robypomper@johnosproject.com>
2021-03-05 13:12:02 +0100

    feat(jcpCommons): Added JSL Status method's definition to APIJSLWBInit class

--- src/jcpCommons/java/com/robypomper/josp/jcp/paths/jslwb/APIJSLWBInit.java
+++ src/jcpCommons/java/com/robypomper/josp/jcp/paths/jslwb/APIJSLWBInit.java
@@ -23,18 +23,21 @@ public class APIJSLWBInit {
     // API's methods
 
     // '{mthdName}
+    private static final String MTHD_JSL_STATUS = "sessionStatus";
     private static final String MTHD_INIT_JSL   = "jsl";
     private static final String MTHD_INIT_SSE   = "sse";
 
     // '/apis/{apiName}/{apiVersion}/{mthdName}
+    public static final String FULL_PATH_JSL_STATUS = API_PATH + "/" + API_VER + "/" + MTHD_JSL_STATUS;
     public static final String FULL_PATH_INIT_JSL   = API_PATH + "/" + API_VER + "/" + MTHD_INIT_JSL;
     public static final String FULL_PATH_INIT_SSE   = API_PATH + "/" + API_VER + "/" + MTHD_INIT_SSE;
 
 
     // API's descriptions
 
-    public static final String DESCR_PATH_INIT_JSL  = "Initialize new JSL Instance for current session";
-    public static final String DESCR_PATH_INIT_SSE  = "Create and return new SSE, if given client* params then it also initialize new JSL Instance for current session";
+    public static final String DESCR_PATH_JSL_STATUS    = "Return the session id and JSL instance status";
+    public static final String DESCR_PATH_INIT_JSL      = "Initialize new JSL Instance for current session";
+    public static final String DESCR_PATH_INIT_SSE      = "Create and return new SSE, if given client* params then it also initialize new JSL Instance for current session";
 
 //@formatter:on
 }
