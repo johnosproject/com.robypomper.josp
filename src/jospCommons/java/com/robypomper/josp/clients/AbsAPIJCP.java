9543fe247c3b116c0f2fa8be8411f041016b034f
Roberto Pompermaier <robypomper@johnosproject.com>
2021-03-15 12:21:26 +0100

    feature(jospCommons): Added status methods to AbsAPIJCP class

--- src/jospCommons/java/com/robypomper/josp/clients/AbsAPIJCP.java
+++ src/jospCommons/java/com/robypomper/josp/clients/AbsAPIJCP.java
@@ -19,6 +19,13 @@
 
 package com.robypomper.josp.clients;
 
+import com.github.scribejava.core.model.Verb;
+import com.robypomper.josp.params.jcp.JCPStatus;
+import com.robypomper.josp.params.jcp.ServiceStatus;
+import com.robypomper.josp.paths.jcp.JCPStatusAbs;
+
+import java.util.List;
+
 public class AbsAPIJCP extends AbsAPI {
 
     public AbsAPIJCP(JCPAPIsClientJCP jcpClient) {
@@ -27,6 +34,53 @@ public class AbsAPIJCP extends AbsAPI {
 
     public JCPAPIsClientJCP getClient() {
         return (JCPAPIsClientJCP) jcpClient;
+    }
+
+
+    // Status methods
+
+    public ServiceStatus getStatusReq() throws JCPClient2.ConnectionException, JCPClient2.AuthenticationException, JCPClient2.ResponseException, JCPClient2.RequestException {
+        return jcpClient.execReq(Verb.GET, JCPStatusAbs.FULL_PATH_STATUS_INSTANCE, ServiceStatus.class, isSecure());
+    }
+
+    public String getStatusOnlineReq() throws JCPClient2.ConnectionException, JCPClient2.AuthenticationException, JCPClient2.ResponseException, JCPClient2.RequestException {
+        return jcpClient.execReq(Verb.GET, JCPStatusAbs.FULL_PATH_STATUS_ONLINE, String.class, isSecure());
+    }
+
+    public JCPStatus.Process getStatusProcessReq() throws JCPClient2.ConnectionException, JCPClient2.AuthenticationException, JCPClient2.ResponseException, JCPClient2.RequestException {
+        return jcpClient.execReq(Verb.GET, JCPStatusAbs.FULL_PATH_STATUS_PROCESS, JCPStatus.Process.class, isSecure());
+    }
+
+    public JCPStatus.Java getStatusJavaReq() throws JCPClient2.ConnectionException, JCPClient2.AuthenticationException, JCPClient2.ResponseException, JCPClient2.RequestException {
+        return jcpClient.execReq(Verb.GET, JCPStatusAbs.FULL_PATH_STATUS_JAVA, JCPStatus.Java.class, isSecure());
+    }
+
+    public List<JCPStatus.JavaThread> getStatusJavaThsReq() throws JCPClient2.ConnectionException, JCPClient2.AuthenticationException, JCPClient2.ResponseException, JCPClient2.RequestException {
+        return jcpClient.execReq(Verb.GET, JCPStatusAbs.FULL_PATH_STATUS_JAVA_THS, List.class, isSecure());
+    }
+
+    public JCPStatus.Os getStatusOSReq() throws JCPClient2.ConnectionException, JCPClient2.AuthenticationException, JCPClient2.ResponseException, JCPClient2.RequestException {
+        return jcpClient.execReq(Verb.GET, JCPStatusAbs.FULL_PATH_STATUS_OS, JCPStatus.Os.class, isSecure());
+    }
+
+    public JCPStatus.CPU getStatusCpuReq() throws JCPClient2.ConnectionException, JCPClient2.AuthenticationException, JCPClient2.ResponseException, JCPClient2.RequestException {
+        return jcpClient.execReq(Verb.GET, JCPStatusAbs.FULL_PATH_STATUS_CPU, JCPStatus.CPU.class, isSecure());
+    }
+
+    public JCPStatus.Memory getStatusMemoryReq() throws JCPClient2.ConnectionException, JCPClient2.AuthenticationException, JCPClient2.ResponseException, JCPClient2.RequestException {
+        return jcpClient.execReq(Verb.GET, JCPStatusAbs.FULL_PATH_STATUS_MEMORY, JCPStatus.Memory.class, isSecure());
+    }
+
+    public JCPStatus.Disks getStatusDisksReq() throws JCPClient2.ConnectionException, JCPClient2.AuthenticationException, JCPClient2.ResponseException, JCPClient2.RequestException {
+        return jcpClient.execReq(Verb.GET, JCPStatusAbs.FULL_PATH_STATUS_DISKS, JCPStatus.Disks.class, isSecure());
+    }
+
+    public JCPStatus.Network getStatusNetworkReq() throws JCPClient2.ConnectionException, JCPClient2.AuthenticationException, JCPClient2.ResponseException, JCPClient2.RequestException {
+        return jcpClient.execReq(Verb.GET, JCPStatusAbs.FULL_PATH_STATUS_NETWORK, JCPStatus.Network.class, isSecure());
+    }
+
+    public List<JCPStatus.NetworkIntf> getStatusNetworkIntfsReq() throws JCPClient2.ConnectionException, JCPClient2.AuthenticationException, JCPClient2.ResponseException, JCPClient2.RequestException {
+        return jcpClient.execReq(Verb.GET, JCPStatusAbs.FULL_PATH_STATUS_NETWORK_INTFS, List.class, isSecure());
     }
 
 }
