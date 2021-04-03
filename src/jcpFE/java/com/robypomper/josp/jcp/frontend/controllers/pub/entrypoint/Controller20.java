953048c110cf22d8030471bd031d446a686807cc
Roberto Pompermaier <robypomper@johnosproject.com>
2021-04-03 15:19:55 +0200

    feat(jcpFE): Added "JCP Front End" Controller classes

--- src/jcpFE/java/com/robypomper/josp/jcp/frontend/controllers/pub/entrypoint/Controller20.java
+++ src/jcpFE/java/com/robypomper/josp/jcp/frontend/controllers/pub/entrypoint/Controller20.java
@@ -1,22 +1,17 @@
-package com.robypomper.josp.jcp.fe.controllers;
+package com.robypomper.josp.jcp.frontend.controllers.pub.entrypoint;
 
+import com.robypomper.josp.jcp.base.controllers.ControllerImpl;
 import com.robypomper.josp.jcp.clients.ClientParams;
-import com.robypomper.josp.jcp.info.JCPFEVersions;
-import com.robypomper.josp.jcp.paths.fe.APIFEEntryPoint;
-import com.robypomper.josp.jcp.paths.jslwb.APIJSLWBInit;
-import com.robypomper.josp.jcp.service.docs.SwaggerConfigurer;
+import com.robypomper.josp.jcp.defs.frontend.pub.entrypoint.Paths20;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.spring.web.plugins.Docket;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -24,10 +19,14 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+
+/**
+ * JCP Front End API - Entry Point 2.0
+ */
 @SuppressWarnings("unused")
-@RestController
-@Api(tags = {APIFEEntryPoint.SubGroupEntryPoint.NAME})
-public class APIFEEntryPointController {
+@RestController(value = Paths20.API_NAME + " " + Paths20.DOCS_NAME)
+@Api(tags = Paths20.DOCS_NAME, description = Paths20.DOCS_DESCR)
+public class Controller20 extends ControllerImpl {
 
     // Internal vars
 
@@ -35,14 +34,12 @@ public class APIFEEntryPointController {
     private final String jslWBUrlPublic;
     private final String clientId;
     private final String clientSecret;
-    @Autowired
-    private SwaggerConfigurer swagger;
 
 
     // Constructor
 
     @Autowired
-    public APIFEEntryPointController(ClientParams params) {
+    public Controller20(ClientParams params) {
         this.jslWBUrlInternal = (params.sslPrivate ? "https://" : "http:") + (params.jslWBHostPrivate) + ":" + params.jslWBPort;
         this.jslWBUrlPublic = (params.sslPublic ? "https://" : "http:") + (params.jslWBHostPublic) + ":" + params.jslWBPort;
         this.clientId = params.clientId;
@@ -50,20 +47,10 @@ public class APIFEEntryPointController {
     }
 
 
-    // Docs configs
-
-    @Bean
-    public Docket swaggerConfig_JCPFEEntryPoint() {
-        SwaggerConfigurer.APISubGroup[] sg = new SwaggerConfigurer.APISubGroup[1];
-        sg[0] = new SwaggerConfigurer.APISubGroup(APIFEEntryPoint.SubGroupEntryPoint.NAME, APIFEEntryPoint.SubGroupEntryPoint.DESCR);
-        return SwaggerConfigurer.createAPIsGroup(new SwaggerConfigurer.APIGroup(APIFEEntryPoint.API_NAME, APIFEEntryPoint.API_VER, JCPFEVersions.API_NAME, sg), swagger.getUrlBaseAuth());
-    }
-
-
     // Methods
 
-    @GetMapping(path = APIFEEntryPoint.FULL_PATH_ENTRYPOINT)
-    @ApiOperation(value = APIFEEntryPoint.DESCR_PATH_ENTRYPOINT)
+    @GetMapping(path = Paths20.FULL_PATH_ENTRYPOINT)
+    @ApiOperation(value = Paths20.DESCR_PATH_ENTRYPOINT)
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "JCP FE Backend entry point url", response = String.class),
             @ApiResponse(code = 401, message = "User not authenticated"),
@@ -73,8 +60,8 @@ public class APIFEEntryPointController {
         return ResponseEntity.ok(jslWBUrlPublic);
     }
 
-    @GetMapping(path = APIFEEntryPoint.FULL_PATH_INIT_JSL_SESSION)
-    @ApiOperation(value = APIFEEntryPoint.DESCR_PATH_INIT_JSL_SESSION)
+    @GetMapping(path = Paths20.FULL_PATH_INIT_JSL_SESSION)
+    @ApiOperation(value = Paths20.DESCR_PATH_INIT_JSL_SESSION)
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "JCP FE Backend entry point", response = Boolean.class),
             @ApiResponse(code = 401, message = "User not authenticated"),
@@ -82,7 +69,7 @@ public class APIFEEntryPointController {
     })
     public ResponseEntity<Boolean> initJSLWebBridgeSession(@RequestParam("session_id") String sessionId) {
         try {
-            URL url = new URL(jslWBUrlInternal + APIJSLWBInit.FULL_PATH_INIT_JSL + "?client_id=" + clientId + "&client_secret=" + clientSecret);
+            URL url = new URL(jslWBUrlInternal + com.robypomper.josp.jcp.defs.jslwebbridge.pub.core.init.Paths20.FULL_PATH_INIT_JSL + "?client_id=" + clientId + "&client_secret=" + clientSecret);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestProperty("Cookie", "JSESSIONID=" + sessionId);
             conn.connect();
