543d6afb52794595f51a445d604a4287c1ecf607
Roberto Pompermaier <robypomper@johnosproject.com>
2021-04-03 15:05:06 +0200

    feat(jospAPIs): Added Versions classes for 1st level API definitions

---
+++ src/jospAPIs/java/com/robypomper/josp/jcp/defs/frontend/pub/Versions.java
@@ -0,0 +1,28 @@
+package com.robypomper.josp.jcp.defs.frontend.pub;
+
+import com.robypomper.josp.consts.APIVersionsAbs;
+
+
+/**
+ * JCP Front End
+ * <p>
+ * API's versions definition for 1st level API.
+ */
+public class Versions extends APIVersionsAbs {
+
+    // Class Constants
+
+    // API Group
+    public static final String API_NAME = "JCP Front End";
+    public static final String API_SERVICE = "JCP Front End";
+    // API SubGroups
+    public static final String API_GROUP = "Main";
+    public static final String API_GROUP_FULL = "";
+    public static final String API_GROUP_DESCR = "";
+    // Urls
+    public static final String API_PATH_BASE = "/apis/pub/frontend";
+    public static final String AUTH_PATH_BASE = "/auth";
+    // Versions
+    public static final String VER_JCP_APIs_2_0 = "2.0";
+
+}
