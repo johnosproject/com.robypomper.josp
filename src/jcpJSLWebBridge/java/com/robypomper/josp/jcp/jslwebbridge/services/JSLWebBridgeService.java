b1bac3d77fd9bf9d4b4420ad9410e15aa2dd5918
Roberto Pompermaier <robypomper@johnosproject.com>
2021-04-03 14:43:37 +0200

    feat(jcpJSLWB): Added HttpSession support to JSLWebBridgeService class

--- src/jcpJSLWebBridge/java/com/robypomper/josp/jcp/jslwebbridge/services/JSLWebBridgeService.java
+++ src/jcpJSLWebBridge/java/com/robypomper/josp/jcp/jslwebbridge/services/JSLWebBridgeService.java
@@ -16,8 +16,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import javax.annotation.PreDestroy;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class JSLWebBridgeService {
@@ -26,6 +31,7 @@ public class JSLWebBridgeService {
 
     private static final Logger log = LoggerFactory.getLogger(JSLWebBridgeService.class);
     private final JSLWebBridge webBridge;
+    private static final Map<String, HttpSession> sessions = new HashMap<>();
 
 
     // Constructors
@@ -69,18 +75,28 @@ public class JSLWebBridgeService {
         ServletListenerRegistrationBean<HttpSessionListener> listenerRegBean = new ServletListenerRegistrationBean<>();
 
         listenerRegBean.setListener(new HttpSessionListener() {
-            public void sessionCreated(HttpSessionEvent se) {
+            public void sessionCreated(HttpSessionEvent hse) {
+                sessions.put(hse.getSession().getId(), hse.getSession());
             }
 
-            public void sessionDestroyed(HttpSessionEvent se) {
-                String sessionId = se.getSession().getId();
+            public void sessionDestroyed(HttpSessionEvent hse) {
+                String sessionId = hse.getSession().getId();
                 log.info(String.format("Terminated session '%s'", sessionId));
                 webBridge.destroyJSL(sessionId);
+                sessions.remove(hse.getSession().getId());
             }
 
         });
 
         return listenerRegBean;
+    }
+
+    public List<HttpSession> getAllSessions() {
+        return new ArrayList<>(sessions.values());
+    }
+
+    public HttpSession getSession(String sessionId) {
+        return sessions.get(sessionId);
     }
 
 }
