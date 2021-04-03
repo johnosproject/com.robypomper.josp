093875084b0faa66609951ac11c1d9cee827e8f7
Roberto Pompermaier <robypomper@johnosproject.com>
2021-04-03 15:44:28 +0200

    refactor(jcpFE): Moved main package com.robypomper.josp.jcp.fe to com.robypomper.josp.jcp.frontend

--- src/jcpFE/java/com/robypomper/josp/jcp/frontend/HTMLUtils.java
+++ src/jcpFE/java/com/robypomper/josp/jcp/frontend/HTMLUtils.java
@@ -1,4 +1,4 @@
-package com.robypomper.josp.jcp.fe;
+package com.robypomper.josp.jcp.frontend;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
