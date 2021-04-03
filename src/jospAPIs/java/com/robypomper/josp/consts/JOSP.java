168b01f4e0b7c8f49317f0f7c798be871966c8d7
Roberto Pompermaier <robypomper@johnosproject.com>
2021-04-03 14:36:32 +0200

    refactor(jospAPIs): Split JOSP class in JOSPConstants (and JCPConstants in jcpCommons module)

--- src/jospAPIs/java/com/robypomper/josp/consts/JOSP.java
+++
@@ -1,7 +0,0 @@
-package com.robypomper.josp.consts;
-
-public class JOSP {
-
-    public static final String ANONYMOUS_USERNAME = "Anonymous";
-    public static final String ANONYMOUS_ID = "00000-00000-00000";
-}
---
+++ src/jospAPIs/java/com/robypomper/josp/consts/JOSPConstants.java
@@ -0,0 +1,19 @@
+package com.robypomper.josp.consts;
+
+public class JOSPConstants {
+
+    // User related constants
+
+    public static final String ANONYMOUS_USERNAME = "Anonymous";
+    public static final String ANONYMOUS_ID = "00000-00000-00000";
+
+    // API related constants
+    public static final String API_HEADER_OBJ_ID = "Object-ID";
+    public static final String API_HEADER_SRV_ID = "Service-ID";
+    public static final String API_HEADER_USR_ID = "User-ID";
+
+
+
+    public static int BYTE_TRANSFORM = 1024 * 1024;;
+
+}
