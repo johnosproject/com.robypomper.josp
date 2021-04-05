04c0caf82da3febc20d24ba61874c72d81c99f48
Roberto Pompermaier <robypomper@johnosproject.com>
2021-04-05 09:41:41 +0200

    fix(jospAPIs): Fixed JOSP Auth API / Keycloak's base path

--- src/jospAPIs/java/com/robypomper/josp/defs/auth/keycloak/Versions.java
+++ src/jospAPIs/java/com/robypomper/josp/defs/auth/keycloak/Versions.java
@@ -9,7 +9,7 @@ public class Versions extends com.robypomper.josp.defs.auth.Versions {
     public static final String API_GROUP_FULL = com.robypomper.josp.defs.auth.Versions.API_GROUP_FULL + " / " + API_GROUP;
     public static final String API_GROUP_DESCR = "";
     // Urls
-    public static final String API_PATH_BASE = com.robypomper.josp.defs.auth.Versions.API_PATH_BASE + "/keycloak";
+    public static final String API_PATH_BASE = com.robypomper.josp.defs.auth.Versions.API_PATH_BASE;    // + "/keycloak";       // Path doesn't respect JCP API convention
     public static final String AUTH_PATH_BASE = com.robypomper.josp.defs.auth.Versions.AUTH_PATH_BASE;
 
 }
