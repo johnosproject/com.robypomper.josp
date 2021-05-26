433672ca9a5f3811da90018adeb7e4158d5a3988
Roberto Pompermaier <robypomper@johnosproject.com>
2021-05-26 18:31:51 +0200

    feat(JCPFEStatic): Added API entities for jcp-jsl-wb module

---
+++ src/jcpFE/js/node/src/jcp-jsl-wb/systems/api-entities/objects/APIStructure.js
@@ -0,0 +1,38 @@
+import APIBooleanState from './components/APIBooleanState'
+import APIRangeState from './components/APIRangeState'
+import APIBooleanAction from './components/APIBooleanAction'
+import APIRangeAction from './components/APIRangeAction'
+import APIContainer from './components/APIContainer';
+
+function enrichObjectStructure(jcpJSLWB, component) {
+    if (component.type=="BooleanState")
+        return new APIBooleanState(jcpJSLWB, component);
+    
+    if (component.type=="RangeState")
+        return new APIRangeState(jcpJSLWB, component);
+
+    if (component.type=="BooleanAction")
+        return new APIBooleanAction(jcpJSLWB, component);
+    
+    if (component.type=="RangeAction")
+        return new APIRangeAction(jcpJSLWB, component);
+
+    if (component.type=="Container") {
+        component.subComps.forEach(function(component, idx, objPerms) {
+            objPerms[idx] = enrichObjectStructure(jcpJSLWB, component);
+        });
+    }
+        
+    return component;
+}
+
+export default class APIStructure extends APIContainer {
+
+    _object = null;
+
+    constructor(jcpJSLWB,object,struct) {
+        super(jcpJSLWB,enrichObjectStructure(jcpJSLWB, struct));
+        this._object = object;
+    }
+
+}
