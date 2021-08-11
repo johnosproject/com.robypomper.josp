f4312c645fc30d6df81b68572f5dd0fe2ae6006c
Roberto Pompermaier <robypomper@johnosproject.com>
2021-08-11 14:30:43 +0200

    feat(jospJOD): Added generic *Shell workers
    Refactored old and deprecated *UnixShell classes as support class for jod.yml configs files on JOSP 2.2.0 version.

--- src/jospJOD/java/com/robypomper/josp/jod/executor/PullerUnixShell.java
+++ src/jospJOD/java/com/robypomper/josp/jod/executor/PullerUnixShell.java
@@ -19,79 +19,18 @@
 
 package com.robypomper.josp.jod.executor;
 
-import com.robypomper.java.JavaExecProcess;
 import com.robypomper.josp.jod.structure.JODComponent;
-import com.robypomper.josp.jod.structure.JODState;
-import com.robypomper.log.Mrk_JOD;
-
-import java.io.IOException;
-import java.util.Map;
 
 
 /**
- * JOD Puller for Unix Shell commands.
- * <p>
- * Each time the {@link #pull()} method is called, it execute set command and parse his command as component's status.
+ * Deprecated class, replaced with {@link PullerShell}.
+ *
+ * @deprecated
  */
-public class PullerUnixShell extends AbsJODPuller {
+public class PullerUnixShell extends PullerShell {
 
-    // Class constants
-
-    private static final String PROP_CMD = "cmd";
-    private static final String PROP_FREQ_SEC = "freq";                 // in seconds
-
-
-    // Internal vars
-
-    private final String cmd;
-    private final int freq_ms;
-
-
-    // Constructor
-
-    /**
-     * Default PullerUnixShell constructor.
-     *
-     * @param name       name of the puller.
-     * @param proto      proto of the puller.
-     * @param configsStr configs string, can be an empty string.
-     */
     public PullerUnixShell(String name, String proto, String configsStr, JODComponent component) throws MissingPropertyException, ParsingPropertyException {
-        super(name, proto, component);
-        log.trace(Mrk_JOD.JOD_EXEC_IMPL, String.format("PullerUnixShell for component '%s' init with config string '%s://%s'.", getName(), proto, configsStr));
-
-        Map<String, String> configs = splitConfigsStrings(configsStr);
-        cmd = parseConfigString(configs, PROP_CMD);
-        freq_ms = parseConfigInt(configs, PROP_FREQ_SEC, Integer.toString(AbsJODPuller.DEF_POLLING_TIME / 1000)) * 1000;
-    }
-
-    protected long getPollingTime() {
-        return freq_ms;
-    }
-
-    // Mngm
-
-    /**
-     * Pull method: print a log message and call the {@link JODState} sub
-     * class's <code>setUpdate(...)</code> method.
-     */
-    @Override
-    public void pull() {
-        log.trace(Mrk_JOD.JOD_EXEC_IMPL, String.format("PullerUnixShell '%s' of proto '%s' pulling (cmd='%s')", getName(), getProto(), cmd));
-
-        // CmdPartitioning
-        String state;
-        try {
-            state = JavaExecProcess.execCmd(cmd).trim();
-
-        } catch (IOException | JavaExecProcess.ExecStillAliveException e) {
-            log.warn(Mrk_JOD.JOD_EXEC_IMPL, String.format("PullerUnixShell error on executing cmd '%s' for component '%s' because '%s'", cmd, getName(), e.getMessage()), e);
-            return;
-        }
-        log.info(Mrk_JOD.JOD_EXEC_IMPL, String.format("PullerUnixShell '%s' of proto '%s' read state '%s'", getName(), getProto(), state));
-
-        if (!convertAndSetStatus(state))
-            log.warn(Mrk_JOD.JOD_EXEC_IMPL, String.format("PullerUnixShell for component '%s' can't update his component because not supported (%s)", getName(), getComponent().getClass().getSimpleName()));
+        super(name, proto, configsStr, component);
     }
 
 }
